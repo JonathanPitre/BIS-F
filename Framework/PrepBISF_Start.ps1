e0e038371b202147488986b17f3a217d15cf66d2
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2026-08-21 12:07:52 -0400

    Correct grammatical errors and improve formatting in PrepBISF_Start.ps1
    Updated the script documentation to fix minor grammatical issues, including corrections to spelling and consistency in variable naming. Enhanced clarity in comments and improved overall readability of the script.

--- Framework/PrepBISF_Start.ps1
+++ Framework/PrepBISF_Start.ps1
@@ -1,11 +1,11 @@
 ﻿<#
 	.SYNOPSIS
-		Prepare BaseImage for Image Management Software like PVS, MCS, VMwareView or ask for Sysprep if nothing of them is installed
+		Prepare Base Image for Image Management Software like PVS, MCS, VMwareView or ask for Sysprep if nothing of them is installed
 	.DESCRIPTION
 		Use the parameters for CLI Switches
-		For slient automation please configure BIS-F with the additional ADMX templates that can be found in your BIS-F installation folder
+		For silent automation please configure BIS-F with the additional ADMX templates that can be found in your BIS-F installation folder
 	.PARAMETER Verbose
-		YES,NO = Enable verbose mode to show supressed messages on console
+		YES,NO = Enable verbose mode to show suppressed messages on console
 	.PARAMETER Debug
 		Enable debug mode for active debugging, for developers only !!
 	.EXAMPLE
@@ -16,18 +16,18 @@
 
 		History:
 		24.09.2012 MS: Script created
-		03.07.2013 MS: Write Scriptpath to registry $hklm_software\PS-SCRIPTS\COMMON to use in hole environment
+		03.07.2013 MS: Write script path to registry $hklm_software\PS-SCRIPTS\COMMON to use in hole environment
 		26.08.2013 MS  Removed $XA_PrpPVS_Folder = $SubCall_Folder + "60_XA_PrpPVS\"
-		16.09.2013 MS: Added customfolder 99_XX_Custom\20_XX_PrepPVS
+		16.09.2013 MS: Added custom folder 99_XX_Custom\20_XX_PrepPVS
 		16.09.2013 MS: Load_PS_Folder -def_load_PS_Folder $LIB_Folder
-		17.09.2013 MS: Remove unused variable and get Foldernames fom LIB_Config
-		18.09.2013 MS: Replace $date with $(Get-date) to get current timestamp at running scriptlines write to the logfile
-		18.09.2013 MS: Predefine $LIB & $Subcall folder
+		17.09.2013 MS: Remove unused variable and get folder names from LIB_Config
+		18.09.2013 MS: Replace $date with $(Get-date) to get current timestamp at running script lines write to the log file
+		18.09.2013 MS: Predefine $LIB_Folder & $SubCall_Folder
 		19.09.2013 MS: IF ($scripts -ne $null)
-		17.12.2013 MS: Added $return for Errorhandling and exiting script
-		28.01.2014 MS: Changed $return = Invoke-Expression $item.FullName to Invoke-Expression $item.FullName for ErrorHandling returncode
-		10.03.2014 BR/MS: ShowMessageBox, review Code
-		10.03.2014 MS: Changed to $psfolder = $SubCall_Folder + "15_XX_Custom\20_XX_PrepPVS"
+		17.12.2013 MS: Added $return for error handling and exiting script
+		28.01.2014 MS: Changed $return = Invoke-Expression $item.FullName to Invoke-Expression $item.FullName for error handling return code
+		10.03.2014 BR/MS: Showmessage box, review Code
+		10.03.2014 MS: Changed to $ScriptsFolder = $SubCall_Folder + "15_XX_Custom\20_XX_PrepPVS"
 		11.03.2014 MS: For ($i = 1; $i -lt 6; $i++) {write-host}
 		11.03.2014 MS: Import-Module $Modules
 		18.03.2014 BR: Reviewed code, implemented central functions
@@ -38,11 +38,11 @@
 		12.08.2014 MS: Changed log file name from .log to .bis (BIS = BaseImageScripts)
 		14.08.2014 MS: Changed ForegroundColor Green Write-Host "Import Modules $Modules" -ForegroundColor Green
 		17.08.2014 MS: Added CLI Switch for Citrix Personal vDisk -> CtxPvd
-		18.08.2014 MS: Added computer name to logfilename $Global:LogFileName = "Prep_BIS_$($computer)_$timestamp.bis"
+		18.08.2014 MS: Added computer name to log file name $Global:LogFileName = "Prep_BIS_$($computer)_$timestamp.bis"
 		15.09.2014 MS: Added CLI Switch -P2PVS to use P2PVS instead of XenConvert if installed
-		15.09.2014 MS: Added CLI switch -verbose -> $LIC_PVS_CLI_VB to show supressed messages in console
+		15.09.2014 MS: Added CLI switch -verbose -> $LIC_PVS_CLI_VB to show suppressed messages in console
 		06.02.2015 MS: Added check if a reboot is pending before continue
-		18.02.2015 MS: Added CLI switch -SupressPndReboot to supress a pending reboot
+		18.02.2015 MS: Added CLI switch -SuppressPndReboot to suppress a pending reboot
 		14.04.2015 MS: Added NoSut CLI Switch to prevent Shutdown after BaseImage successfully build
 		05.05.2015 MS: Added check for local execution
 		18.05.2015 MS: Added CLI Switch VERYSILENT handling
@@ -51,33 +51,32 @@
 		13.08.2015 MS: Added CLI Switch FSXRulesShare to define a central unc share for fsLogix rules and assignments
 		21.08.2015 MS: Change Request 77 - remove all XX,XA,XD from al files and Scripts
 		04.11.2015 MS: Added CLI option DelAllUsersStartmenu to delete all Objects in C:\ProgramData\Microsoft\Windows\Start Menu\*
-		15.12.2015 MS: Added CLI option DisableIPv6 to disable IPv6 completly
+		15.12.2015 MS: Added CLI option DisableIPv6 to disable IPv6 completely
 		28.01.2016 MS: Added CLI option Sysprep to use them of No Image Management Software detected
-		10.03.2016 MS: Added CLI switch DisableConcoleCheck to disable the check if preparation phase is running in console session
-		17.03.2016 MS: Added CLI option TurboUpdate to update Turbo.net Supscription on system startup
+		10.03.2016 MS: Added CLI switch DisableConsoleCheck to disable the check if preparation phase is running in console session
+		17.03.2016 MS: Added CLI option TurboUpdate to update Turbo.net Subscription on system startup
 		17.03.2016 MS: Added CLI option DelProf to delete unused profiles, delprof2.exe must be download first and save in the BIS-F Tools Folder
 		04.10.2016 MS: Renamed folder names for global architectural re-design
 		23.11.2016 MS: Added CLI option vmOSOT to run VMware OS Optimization Tool if installed in any folder on local drive (C:) only. Running with the default template
 		23.11.2016 MS: Added CLI command 'WEMAgentBrokerName' to set the Citrix Workspace Environment Agent BrokerName if not configured via GPO
 		10.01.2017 MS: Added CLI command 'XAImagePrepRemoval' during Prepare XenApp for Provisioning you can remove RemoveCurrentServer and ClearLocalDatabaseInformation, this would be set with this Parameter or prompted to administrator to choose
-		11.01.2017 MS: Added Cli command 'RESWASdisableBaseImage' to disable RES ONE Automation Agent on Base Image only to prevent RES ONE License usage for your Base Iamges
+		11.01.2017 MS: Added Cli command 'RESWASdisableBaseImage' to disable RES ONE Automation Agent on Base Image only to prevent RES ONE License usage for your Base Image
 		27.01.2017 MS: Bug fix 149; Added $Global:LIC_BISF_CLI_LSb="" to define the variable, required for the ADMX templates
 		02.01.2017 MS: remove CLI parameters exclude -DebugMode and -Verbose, thi can be configured with the ADMX File
 		14.08.2017 MS: add cli switch ExportSharedConfiguration to export BIS-F ADMX Reg Settings into an XML File
 		11.09.2017 MS: Getting PersSate from BISF Registry to control running prep after pers is finished
-		12.09.2017 MS: using progressbar during wait for the personlization is finished
-		16.10.2017 MS: Bugfix detecting wrong POSH Version if running BIS-F remotly, using $PSVersionTable.PSVersion.Major, thx to Fabian Danner
-		12.07.2018 MS: Bugfix 40: PendingReboot - give a empty value back
-		13.08.2019 MS: ENH 121 - change filenameextension from bis to log
-		14.08.2019 MS: FRQ 3 - Remove Messagebox and using default setting if GPO is not configured
+		12.09.2017 MS: using progress bar during wait for the personalization is finished
+		16.10.2017 MS: Fixed detecting wrong POSH Version if running BIS-F remotely, using $PSVersionTable.PSVersion.Major, thx to Fabian Danner
+		12.07.2018 MS: Fixed 40: PendingReboot - give a empty value back
+		13.08.2019 MS: ENH 121 - change file name extension from bis to log
+		14.08.2019 MS: FRQ 3 - Remove message box and using default setting if GPO is not configured
 		21.09.2019 MS: ENH 127 - Personalization is in Active State Override
 		05.10.2019 MS: ENH 144 - Enable Powershell Transcript
 		06.10.2019 MS: Removing call to Get-BISFScriptExecutionPath
-		08.10.2019 MS: ENH 146 - Removing Test for Get-PendingReboot out of this script and included into BISF.ps1 to an eralier state
+		08.10.2019 MS: ENH 146 - Removing Test for Get-PendingReboot out of this script and included into BISF.ps1 to an earlier state
 		20.01.2020 MS: HF 191 - MaxExecution is not set correctly
-		18.02.2020 JK: Grammar fixup		
-	.LINK
-		https://eucweb.com
+		18.02.2020 JK: Grammar fixup
+		13.08.2026 JP: Close Fluent splash after Initialize-BISFConfiguration (no 5s dead wait)
 #>
 
 [CmdletBinding(SupportsShouldProcess = $true)]
@@ -91,27 +90,25 @@ Begin {
 	$error.Clear()
 	If ( $TerminateScript -is [system.object] ) { Remove-Variable TerminateScript }
 	Clear-Host
-	$computer = gc env:computername
-	$timestamp = Get-Date -Format yyyyMMdd-HHmmss
-	$Regpath = "HKLM:\SOFTWARE\Policies\Login Consultants\BISF"
+	$Computer = $env:COMPUTERNAME
+	$Timestamp = Get-Date -Format yyyyMMdd-HHmmss
+	$RegPath = "HKLM:\SOFTWARE\Policies\Login Consultants\BISF"
 
 	## ENH 144 - Powershell Transcript
-	$ERRORACTIONPREFERENCE = "STOP"
 	try {
-		$WPTEnabled = (Get-ItemProperty $Regpath -Name "LIC_BISF_CLI_LOG_WPT").LIC_BISF_CLI_LOG_WPT
+		$WPTEnabled = (Get-ItemProperty $RegPath -Name "LIC_BISF_CLI_LOG_WPT" -ErrorAction Stop).LIC_BISF_CLI_LOG_WPT
 	}
-	catch { }
+	catch { $WPTEnabled = $null }
 
 	IF ($WPTEnabled -eq 1) {
-		$Global:WPTlog = "C:\Windows\Logs\PREP_BISF_WPT_$($computer)_$timestamp.log"
+		$Global:WPTLog = "C:\Windows\Logs\PREP_BISF_WPT_$($Computer)_$Timestamp.log"
 		Start-Transcript $WPTLog | Out-Null
 	}
-	$ERRORACTIONPREFERENCE = "Continue"
 
-	# Setting default variables ($PSScriptroot/$logfile/$PSCommand,$PSScriptFullname/$scriptlibrary/LogFileName) independent on running script from console or ISE and the powershell version.
+	# Setting default variables ($PSScriptRoot/$LogFile/$PSCommand,$PSScriptFullname/$ScriptLibrary/LogFileName) independent on running script from console or ISE and the powershell version.
 	If ($($host.name) -like "* ISE *") {
 		# Running script from Windows Powershell ISE
-		$PSScriptFullName = $psise.CurrentFile.FullPath.ToLower()
+		$PSScriptFullName = $psISE.CurrentFile.FullPath.ToLower()
 		$PSCommand = (Get-PSCallStack).InvocationInfo.MyCommand.Definition
 	}
 	ELSE {
@@ -127,9 +124,9 @@ Begin {
 	$Global:Main_Folder = $PSScriptRoot
 	$Global:SubCall_Folder = $PSScriptRoot + "\SubCall\"
 	$Global:LIB_Folder = $SubCall_Folder + "Global\"
-	$Global:LogFileName = "PREP_BISF_$($computer)_$timestamp.log"
+	$Global:LogFileName = "PREP_BISF_$($Computer)_$Timestamp.log"
 	$Global:LOGFile = "C:\Windows\Logs\$LogFileName"
-	$Global:LOG = $LOGFile
+	$Global:LOG = $LogFile
 	$Global:ExportSharedConfiguration = $ExportSharedConfiguration
 	$Global:LIC_BISF_CLI_DB = $DebugMode #DB = debug
 	$Global:LIC_BISF_CLI_DF = @() #DF = Defrag
@@ -145,12 +142,12 @@ Begin {
 	$Global:LIC_BISF_CLI_SB = @() #SB = Shutdown
 	$Global:LIC_BISF_CLI_SR = @() #SR = SuppressPendingReboot
 	$Global:LIC_BISF_CLI_VS = @() #VS = VerySilent
-	$Global:LIC_BISF_CLI_FS = @() #FS = FS logix Delete Rules
-	$Global:LIC_BISF_CLI_RS = @() #RS = Fs logix Rules Share
-	$GLobal:LIC_BISF_CLI_SM = @() #SM = Delete All Users Start Menue
-	$GLobal:LIC_BISF_CLI_V6 = @() #V6 = Disable IPV6
-	$GLobal:LIC_BISF_CLI_SP = @() #SP = Sysprep
-	$GLobal:LIC_BISF_CLI_DP = @() #DP = DelProf2
+	$Global:LIC_BISF_CLI_FS = @() #FS = FSLogix Delete Rules
+	$Global:LIC_BISF_CLI_RS = @() #RS = FSLogix Rules Share
+	$Global:LIC_BISF_CLI_SM = @() #SM = Delete All Users Start Menu
+	$Global:LIC_BISF_CLI_V6 = @() #V6 = Disable IPV6
+	$Global:LIC_BISF_CLI_SP = @() #SP = Sysprep
+	$Global:LIC_BISF_CLI_DP = @() #DP = DelProf2
 	$Global:LIC_BISF_CLI_ST = @() #ST = SessionType
 	$Global:LIC_BISF_CLI_LS = @() #LS = BISF LogShare
 	$Global:LIC_BISF_CLI_TB = @() #TB = Turbo.net Update
@@ -164,7 +161,7 @@ Begin {
 }
 
 Process {
-	#check Powershell minimun version 3.0
+	#check Powershell minimum version 3.0
 	$PSverMin = "3"
 	If (-not ($PSVersionTable.PSVersion.Major -ge $PSverMin)) {
 		Write-Host "Powershell v$PSverMin or higher is required for BIS-F, detected version: $($PSVersionTable.PSVersion.Major)" -ForegroundColor white -BackgroundColor red
@@ -175,28 +172,30 @@ Process {
 	#load BISF Modules
 	try {
 		$Modules = @(Get-ChildItem -path $LIB_Folder -filter "*.psd1" -Force)
-		ForEach ($module in $Modules) {
-			$modulename = (Test-ModuleManifest $($Module.FullName) -Verbose:$false).Name
-			$global:mainmodulename = $modulename
-			$modulecompany = (Test-ModuleManifest $($Module.FullName) -Verbose:$false).CompanyName
-			Write-Host "--- Importing Module $modulename ---" -ForegroundColor Green -BackgroundColor DarkGray
+		ForEach ($Module in $Modules) {
+			$ModuleName = (Test-ModuleManifest $($Module.FullName) -Verbose:$false).Name
+			$Global:MainModuleName = $ModuleName
+			$ModuleCompany = (Test-ModuleManifest $($Module.FullName) -Verbose:$false).CompanyName
+			Write-Host "--- Importing Module $ModuleName ---" -ForegroundColor Green -BackgroundColor DarkGray
 			Import-Module -Name $($Module.FullName) -Force
 		}
 	}
 	catch {
-		Throw "An error occured while loading modules. The error is: $_"
+		Throw "An error occurred while loading modules. The error is: $_"
 		Exit 1
 	}
 
-	# 10.10.2019 Show Splashscreen
+	# 10.10.2019 Show splash screen
+	# 13.08.2026 JP: Fluent dialog stays up while configuration initializes
 	Show-BISFSplashScreen
 
 	# Initialize all variables used by BISF
 	Initialize-BISFConfiguration
+	Close-BISFSplashScreen
 
 
 	#ENH 127 - Personalization is in Active State Override
-	IF ($LIC_BISF_CLI_PersonalizationOverrideTimeOut -eq $null) {
+	IF ($null -eq $LIC_BISF_CLI_PersonalizationOverrideTimeOut) {
 		[int]$MaximumExecutionMinutes = 60
 		Write-BISFLog "Maximum execution time will default override with the value of $MaximumExecutionMinutes minutes"
 	}
@@ -215,11 +214,11 @@ Process {
 		if ((Get-Date) -ge $MaximumExecutionTime) {
 			Write-BISFLog -Msg "The operation has exceeded the maximum execution time of $MaximumExecutionMinutes Minutes." -Type W
 			$PersState = $TaskStates[3]
-			Write-BISFLog -Msg "Write PersState to registry location Path: $hklm_software_LIC_CTX_BISF_SCRIPTS -Name: LIC_BISF_PersState -Value: $PersState"
-			Set-ItemProperty -Path $hklm_software_LIC_CTX_BISF_SCRIPTS -Name "LIC_BISF_PersState" -value "$PersState" -Force
+			Write-BISFLog -Msg "Write PersState to registry location Path: $HklmBisfScripts -Name: LIC_BISF_PersState -Value: $PersState"
+			Set-ItemProperty -Path $HklmBisfScripts -Name "LIC_BISF_PersState" -Value "$PersState" -Force
 		}
 
-		$PersState = (Get-ItemProperty $hklm_software_LIC_CTX_BISF_SCRIPTS -Name "LIC_BISF_PersState").LIC_BISF_PersState
+		$PersState = (Get-ItemProperty $HklmBisfScripts -Name "LIC_BISF_PersState").LIC_BISF_PersState
 		IF (($PersState -eq $($TaskStates[0])) -or ($PersState -eq $($TaskStates[3]))) {
 			$a = 100
 			Write-Progress -Activity "Personalization is in current ""$PersState"" state, executing the preparation task in 3 seconds" -PercentComplete $a -Status "Finish."
@@ -240,26 +239,22 @@ Process {
 	# create RegHive if needed
 	$BISFRegHive = Test-BISFRegHive -Verbose:$VerbosePreference
 
-
-	# Set CLI Options in registry location
-	#Set-BISFCLIinReg  ##<<< please do not active, currently buggy MS 10.08.2015
-
 	#Load Global environment
-	$psfolder = $LIB_Folder
-	Invoke-BISFFolderScripts -Path "$psfolder" -Verbose:$VerbosePreference
+	$ScriptsFolder = $LIB_Folder
+	Invoke-BISFFolderScripts -Path "$ScriptsFolder" -Verbose:$VerbosePreference
 
 
 	Add-BISFFinishLine
 
 	#Load custom scripts
-	$psfolder = $SubCall_Folder + "Preparation\Custom"
-	Invoke-BISFFolderScripts -Path "$psfolder" -Verbose:$VerbosePreference
+	$ScriptsFolder = $SubCall_Folder + "Preparation\Custom"
+	Invoke-BISFFolderScripts -Path "$ScriptsFolder" -Verbose:$VerbosePreference
 
 	Add-BISFFinishLine
 
 	#load Preparation Scripts
-	$psfolder = $SubCall_Folder + "Preparation"
-	Invoke-BISFFolderScripts -Path "$psfolder" -Verbose:$VerbosePreference
+	$ScriptsFolder = $SubCall_Folder + "Preparation"
+	Invoke-BISFFolderScripts -Path "$ScriptsFolder" -Verbose:$VerbosePreference
 
 }
 
@@ -268,15 +263,15 @@ End {
 		Write-BISFLog -Msg "- - - End Of Script - - - "
 		#unload BISF Modules
 		$Modules = @(Get-ChildItem -path $LIB_Folder -filter "*.psd1" -Force)
-		ForEach ($module in $Modules) {
-			$modulename = (Test-ModuleManifest $($Module.FullName)).Name
-			$modulecompany = (Test-ModuleManifest $($Module.FullName)).CompanyName
-			Write-Host "--- Removing Module $modulename ---" -ForegroundColor Green -BackgroundColor DarkGray
-			Remove-Module -Name $modulename -Force -ErrorAction Stop
+		ForEach ($Module in $Modules) {
+			$ModuleName = (Test-ModuleManifest $($Module.FullName)).Name
+			$ModuleCompany = (Test-ModuleManifest $($Module.FullName)).CompanyName
+			Write-Host "--- Removing Module $ModuleName ---" -ForegroundColor Green -BackgroundColor DarkGray
+			Remove-Module -Name $ModuleName -Force -ErrorAction Stop
 		}
 	}
 	catch {
-		Throw "An error occured while unloading modules. The error is:`r`n$_"
+		Throw "An error occurred while unloading modules. The error is:`r`n$_"
 		Exit 1
 	}
 	IF ($WPTEnabled -eq 1) { Stop-Transcript -ErrorAction SilentlyContinue | Out-Null }
