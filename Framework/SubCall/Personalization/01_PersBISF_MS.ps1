c1b6d1b417f037c868cffc97c8d97123284fb7d2
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2026-08-23 18:49:30 -0400

    Refactor 01_PersBISF_MS.ps1 for improved readability and consistency
    Updated conditional statements to use consistent syntax and improved logging messages for clarity. This refactor enhances the overall readability of the script while maintaining its functionality.

--- Framework/SubCall/Personalization/01_PersBISF_MS.ps1
+++ Framework/SubCall/Personalization/01_PersBISF_MS.ps1
@@ -17,7 +17,6 @@
 		21.09.2019 MS: ENH 9 - LAPS Support for Non-Persistent VDI
 		04.08.2020 MS: HF 271 - 00_PersBISF_WriteCacheDisk.ps1 fails, due to timing issue with registry values
 		24.11.2020 MS: HF 285 - Join AAD if enabled in GPO
-
 #>
 
 Begin {
@@ -29,8 +28,8 @@ Begin {
 
 Process {
 
-	IF (!($CTXAppLayerName -eq "OS-Layer")) {
-		IF ($LIC_BISF_CLI_LAPSExpirationTime -eq "YES" ) { Set-BISFLAPSExpirationTime }
+	if ($CTXAppLayerName -ne 'OS-Layer') {
+		if ($LIC_BISF_CLI_LAPSExpirationTime -eq 'YES') { Set-BISFLAPSExpirationTime }
 		# Resync Time with Domain
 		Write-BISFLog -Msg "Syncing Time from Domain"
 		& "$env:SystemRoot\system32\w32tm.exe" /config /update
@@ -40,15 +39,16 @@ Process {
 		Write-BISFLog -Msg "Apply Computer GPO" -showConsole -Color Cyan
 		Start-BISFProcWithProgBar -ProcPath "$env:SystemRoot\system32\gpupdate.exe" -Args "/Target:Computer /Force /Wait:0" -ActText "Apply Computer GPO"
 
-		IF ($LIC_BISF_CLI_MS_AAD_HybridJoinb -eq "YES") {
+		if ($LIC_BISF_CLI_MS_AAD_HybridJoinb -eq 'YES') {
 			Write-BISFLog -Msg "Join Azure Active Dirctory " -showConsole -Color Cyan
 			Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\dsregcmd.exe" -Args "/join" -ActText "Join Azure AD Domain"
 			Start-BISFProcWithProgBar -ProcPath "$env:windir\system32\dsregcmd.exe" -Args "/status" -ActText "Get Azure AD Domain status"
-		} ELSE {
+		}
+		else {
 			Write-BISFLog -Msg "Join Azure Active Directory is NOT enabled in BIS-F GPO" -showConsole -Color Cyan
 		}
 	}
-	ELSE {
+	else {
 		Write-BISFLog -Msg "Do nothing in AppLayering $CTXAppLayerName"
 	}
 }
