dcaf3ed01a4a289fbd52178e27c5c2ede2f30e46
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2026-08-23 14:11:40 -0400

    Correct grammatical errors and improve documentation in 80_PrepBISF_WriteCacheDisk.ps1
    Updated comments to fix spelling and grammatical issues, enhancing clarity and consistency throughout the script's documentation. Adjusted variable naming for uniformity and improved overall readability.

--- Framework/SubCall/Preparation/80_PrepBISF_WriteCacheDisk.ps1
+++ Framework/SubCall/Preparation/80_PrepBISF_WriteCacheDisk.ps1
@@ -9,44 +9,42 @@ param(
 	.NOTES
 		Author: Matthias Schlimm
 		Editor: Mike Bijl (Rewritten variable names and script format)
-		Company:  EUCWeb.com
+
 
 		History:
 		28.02.2013 MS: Script created
 		07.03.2013 MS: Read from diskpart, error to read substring, write empty uniqueID to registry
-		25.06.2013 MS: Change location for temporary Diskpartfile to %TEMP%
+		25.06.2013 MS: Change location for temporary Diskpart file to %TEMP%
 		12.09.2013 MS: Critical fix to get uniqueid on english display language only
-		18.09.2013 MS: replace $date with $(Get-date) to get current timestamp at running scriptlines write to the logfile
+		18.09.2013 MS: replace $date with $(Get-date) to get current timestamp at running script lines write to the log file
 		01.10.2013 MS: add function SetRefSrv - Set Reference Server Hostname in registry to detect it in the personalize script to skip reboot
 		03.03.2014 BR: Revisited Script
 		18.03.2014 BR: revisited Script
 		21.03.2014 MS: add setCDROM, last code change before release to web
-		13.08.2014 MS: remove $logfile = Set-logFile, it would be used in the 10_XX_LIB_Config.ps1 Script only
-		13.08.2014 MS: Check if $returnCheckPVSSysVariable exists, then get uniqueID from persitend drive and set it to registry
+		13.08.2014 MS: remove $LogFile = Set-LogFile, it would be used in the 10_XX_LIB_Config.ps1 Script only
+		13.08.2014 MS: Check if $returnCheckPVSSysVariable exists, then get uniqueID from persistent drive and set it to registry
 		20.08.2014 MS: add line 70 -> get-LogContent -GetLogFile "$DiskpartFile"
 		31.10.2014 MB: renamed variable: returnCheckPVSSysVariable -> returnTestPVSEnvVariable
 		01.10.2015 MS: rewritten script to use central BISF function
-		10.01.2017 MS: BugFix 134- PrepareWriteCacheDisk: add space on either side of the Driveletter variable $searvol, thx to Jeremy Saunders
-		10.01.2017 MS: BugFix 134: PrepareWriteCacheDisk: MBR disk with 8 characters to get the right uniqueID from Diskpart only, PVS does not support GPT disk, see https://support.citrix.com/article/CTX139478 thx to Jeremy Saunders
-		04.03.2017 MS: BugFix: DiskID is not language neutral, split string after ":" to read the right side only
+		10.01.2017 MS: Fixed 134- PrepareWriteCacheDisk: add space on either side of the DriveLetter variable $SearVol, thx to Jeremy Saunders
+		10.01.2017 MS: Fixed 134: PrepareWriteCacheDisk: MBR disk with 8 characters to get the right uniqueID from Diskpart only, PVS does not support GPT disk, see https://support.citrix.com/article/CTX139478 thx to Jeremy Saunders
+		04.03.2017 MS: Fixed: DiskID is not language neutral, split string after ":" to read the right side only
 		29.07.2017 MS: Feature Request 192: support GPT WriteCacheDisk
 		25.08.2019 MS: ENH 128 - Disable any command if WriteCacheDisk is set to NONE
-		05.10.2019 MS: HF 69 - If WriteCache disk on master is GPT-partiton then uniqueid doesn't match
+		05.10.2019 MS: HF 69 - If WriteCache disk on master is GPT partition then uniqueid doesn't match
 		18.02.2020 JK: Fixed Log output spelling
 		18.01.2021 MS: using PoSh standard verbs for functions
 
-	.LINK
-		https://eucweb.com
 	#>
 
 Begin {
 
 	####################################################################
-	# define environment
-	# Setting default variables ($PSScriptroot/$logfile/$PSCommand,$PSScriptFullname/$scriptlibrary/LogFileName) independent on running script from console or ISE and the powershell version.
+	# Define environment
+	# Setting default variables ($PSScriptRoot/$LogFile/$PSCommand,$PSScriptFullname/$ScriptLibrary/LogFileName) independent on running script from console or ISE and the powershell version.
 	If ($($host.name) -like "* ISE *") {
 		# Running script from Windows Powershell ISE
-		$PSScriptFullName = $psise.CurrentFile.FullPath.ToLower()
+		$PSScriptFullName = $psISE.CurrentFile.FullPath.ToLower()
 		$PSCommand = (Get-PSCallStack).InvocationInfo.MyCommand.Definition
 	}
 	ELSE {
@@ -56,11 +54,11 @@ Begin {
 	[string]$PSScriptName = (Split-Path $PSScriptFullName -leaf).ToLower()
 	If (($PSScriptRoot -eq "") -or ($PSScriptRoot -eq $null)) { [string]$PSScriptRoot = (Split-Path $PSScriptFullName).ToLower() }
 
-	$SysDrive = $env:systemdrive
-	$reg_value_UniqueID = "LIC_BISF_UniqueID_Disk"
-	$reg_value_RefSrv_Hostname = "LIC_BISF_RefSrv_Hostname"
+	$SystemDrive = $env:SystemDrive
+	$RegValueUniqueId = "LIC_BISF_UniqueID_Disk"
+	$RegValueRefSrvHostname = "LIC_BISF_RefSrv_Hostname"
 	$PVSDiskLabel = "PVSWriteCacheDisk"
-	$DiskpartFile = "C:\Windows\Temp\$computer-DiskpartFile.txt"
+	$DiskpartFile = "C:\Windows\Temp\$Computer-DiskpartFile.txt"
 
 	####################################################################
 	####### functions #####
@@ -71,36 +69,46 @@ Begin {
 		GetUniqueID
 
 		.DESCRIPTION
-		Write the UnqiueID of the CacheDisk to the registry
+		Write the UniqueID of the CacheDisk to the registry
 		to use it later on the cloned devices
 
 
 		.NOTES
 		Author: Matthias Schlimm
 
-		Company:  EUCWeb.com
+
 
 		History:
 		dd.mm.yyyy MS: Script created
 		18.01.2021 MS: HF 302 using function Get-BISFDiskID instead of the same code here
 
 		#>
-		Get-BISFDiskID -Driveletter $PVSDiskDrive -ThrowWhenNotFound $False
-		Write-BISFLog -Msg "Set uniqueID $getid for volume $VolNbr / Driveletter $PVSDiskDrive to Registry $hklm_software_LIC_CTX_BISF_SCRIPTS"
-		Set-ItemProperty -Path $hklm_software_LIC_CTX_BISF_SCRIPTS -Name $reg_value_UniqueID -value $DiskID -ErrorAction SilentlyContinue
+		Get-BISFDiskID -DriveLetter $PVSDiskDrive -ThrowWhenNotFound $False
+		Write-BISFLog -Msg "Set uniqueID $GetID for volume $VolNbr / DriveLetter $PVSDiskDrive to Registry $HklmBisfScripts"
+		Set-ItemProperty -Path $HklmBisfScripts -Name $RegValueUniqueId -Value $DiskID -ErrorAction SilentlyContinue
 	}
 
 	function Set-ReferenceServer {
+		[CmdletBinding(SupportsShouldProcess = $true)]
+		param()
+
 		# Set Reference Server Hostname in registry to detect it in the personalize script to skip reboot
-		Write-BISFLog -Msg "Write Reference Server Hostname $computer to Registry $hklm_software_LIC_CTX_BISF_SCRIPTS"
-		Set-ItemProperty -Path $hklm_software_LIC_CTX_BISF_SCRIPTS -Name $reg_value_RefSrv_Hostname -value $computer -ErrorAction SilentlyContinue
+		Write-BISFLog -Msg "Write Reference Server Hostname $Computer to Registry $HklmBisfScripts"
+		if ($PSCmdlet.ShouldProcess("$HklmBisfScripts\$RegValueRefSrvHostname", 'Set reference server hostname')) {
+			Set-ItemProperty -Path $HklmBisfScripts -Name $RegValueRefSrvHostname -Value $Computer -ErrorAction SilentlyContinue
+		}
 	}
 
 	function Set-OpticalDrive {
+		[CmdletBinding(SupportsShouldProcess = $true)]
+		param()
+
 		$CDrom = Get-CimInstance -ClassName Win32_Volume -Filter "DriveType = 5"
-		$CDromDriveletter = $CDrom.Driveletter
-		Set-ItemProperty -Path $hklm_software_LIC_CTX_BISF_SCRIPTS -Name "LIC_BISF_OptDrive" -Value $CDromDriveletter
-		Write-BISFLog -Msg "set optical driveletter $CDromDriveletter"
+		$CDromDriveLetter = $CDrom.DriveLetter
+		if ($PSCmdlet.ShouldProcess("$HklmBisfScripts\LIC_BISF_OptDrive", "Set optical DriveLetter $CDromDriveLetter")) {
+			Set-ItemProperty -Path $HklmBisfScripts -Name "LIC_BISF_OptDrive" -Value $CDromDriveLetter
+			Write-BISFLog -Msg "set optical DriveLetter $CDromDriveLetter"
+		}
 	}
 
 	####################################################################
@@ -111,7 +119,7 @@ Process {
 
 	#### Main Program
 	IF (!($LIC_BISF_CLI_WCD -eq "NONE")) {
-		IF ($returnTestPVSEnvVariable -eq $true) {
+		IF ($ReturnTestPVSEnvVariable -eq $true) {
 			Get-UniqueDiskID
 		}
 		ELSE {
