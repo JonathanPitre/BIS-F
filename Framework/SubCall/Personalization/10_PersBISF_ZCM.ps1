0d71081d9aade83df2508565adf36ccfc6ba2cc7
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2026-08-24 01:26:06 -0400

    Refactor 10_PersBISF_ZCM.ps1 for improved variable naming and logging clarity. This update enhances variable names for consistency, corrects typos in comments and logging messages, and improves overall readability and maintainability of the script while preserving its functionality.

--- Framework/SubCall/Personalization/10_PersBISF_ZCM.ps1
+++ Framework/SubCall/Personalization/10_PersBISF_ZCM.ps1
@@ -5,29 +5,27 @@
 	.EXAMPLE
 	.NOTES
 		Author: Benjamin Ruoff
-	  	Company:  EUCWeb.com
+
 
 		History:
 	  	04.03.2014 BR: Script created
 		11.03.2014 MS: IF (Test-Path ("C:\Program Files (x86)\Novell\ZENworks\bin\zac.exe"))
 		21.03.2014 MS: last code change before release to web
 		12.05.2014 MS: Change from $ZCMConfigPath = "D:\ZCM\" to $ZCMConfigPath = "$PVSDiskDrive\ZCM\"
-		12.05.2014 MS: get ZCM Argumentlist from custom specified registry value -->> $LIC_PVS_ZCM_CFG
-		13.08.2014 MS: remove $logfile = Set-logFile, it would be used in the 10_XX_LIB_Config.ps1 Script only
+		12.05.2014 MS: get ZCM argument list from custom specified registry value -->> $LIC_PVS_ZCM_CFG
+		13.08.2014 MS: remove $LogFile = Set-LogFile, it would be used in the 10_XX_LIB_Config.ps1 Script only
 		15.08.2014 MS: Add Else condition -> Write-BISFLog -Msg "ZENworks Configuration Management not installed"
-		17.08.2014 MS: rewrite script for 32 bit and 64 bit, use $ProgramFilesx86 from function get-osinfo instead of hardcoded path
+		17.08.2014 MS: rewrite script for 32 bit and 64 bit, use $ProgramFilesx86 from function Get-OSInfo instead of hardcoded path
 		10.02.2015 MS: rename syntax from PVS to BISF - $LIC_PVS_ZCM_CFG -> $LIC_BISF_ZCM_CFG
 		06.10.2015 MS: rewritten script with standard .SYNOPSIS
 		12.03.2017 MS: using $LIC_BISF_CLI_ZCM to configure ZCM with ADMX
 		29.10.2017 MS: replace $DiskMode -eq "VDAShared", instead of MCSShared
-
-	.LINK
-		https://eucweb.com
+		23.08.2026 JP: Skip zac register when ADMX POL_ZCM (LIC_BISF_CLI_ZCM) is empty
 #>
 
 Begin {
 	$Product = "Novell ZCM Agent"
-	$servicename = "Novell ZENworks Agent Service"
+	$ServiceName = "Novell ZENworks Agent Service"
 	$ZCMConfigPath = "$PVSDiskDrive\ZCM\"
 	$ZCMConfigFiles = "DeviceData", "DeviceGUID", "initial-web-service"
 	$PSScriptFullName = $MyInvocation.MyCommand.Path
@@ -41,20 +39,20 @@ Process {
 	####### functions #####
 	####################################################################
 	function CheckConfigFiles {
-		$result = $true
-		foreach ($file in $ZCMConfigFiles) {
-			if (!(Test-Path -Path $ZcmConfigPath$file -PathType Leaf)) {
-				$result = $false
+		$Result = $true
+		foreach ($File in $ZCMConfigFiles) {
+			if (!(Test-Path -Path $ZcmConfigPath$File -PathType Leaf)) {
+				$Result = $false
 			}
 		}
-		return $result
+		return $Result
 	}
 
 	####################################################################
 
 	#### Main Program
-	$svc = Test-BISFService -ServiceName "$servicename" -ProductName "$product"
-	IF ($svc -eq $true) {
+	$Svc = Test-BISFService -ServiceName "$ServiceName" -ProductName "$Product"
+	IF ($Svc -eq $true) {
 		# Check Disk Mode
 		$DiskMode = Get-BISFDiskMode
 
@@ -68,15 +66,20 @@ Process {
 				Write-BISFLog -Msg "Starting ZCM Agent"
 				Start-Service -Name 'Novell ZENworks Agent Service' -PassThru
 
-				Write-BISFLog -Msg "Registering ZCM Agent with Arguments $LIC_BISF_CLI_ZCM"
-				Start-Process -FilePath "$ProgramFilesx86\Novell\ZENworks\bin\zac.exe" -ArgumentList $LIC_BISF_CLI_ZCM
+				If ([string]::IsNullOrWhiteSpace($LIC_BISF_CLI_ZCM)) {
+					Write-BISFLog -Msg "ADMX POL_ZCM is not configured (LIC_BISF_CLI_ZCM empty); skipping ZCM register" -Type W
+				}
+				Else {
+					Write-BISFLog -Msg "Registering ZCM Agent with Arguments $LIC_BISF_CLI_ZCM"
+					Start-Process -FilePath "$ProgramFilesx86\Novell\ZENworks\bin\zac.exe" -ArgumentList $LIC_BISF_CLI_ZCM
+				}
 
 				# Wait 3 Minutes before File Backup
-				sleep -Seconds 180
+				Start-Sleep -Seconds 180
 
 				Write-BISFLog -Msg "Backup Config Files to $ZCMConfigPath"
-				foreach ($file in $ZCMConfigFiles) {
-					Copy-Item -Path "$ProgramFilesx86\Novell\ZENworks\conf\$file" -Destination $ZCMConfigPath -Force
+				foreach ($File in $ZCMConfigFiles) {
+					Copy-Item -Path "$ProgramFilesx86\Novell\ZENworks\conf\$File" -Destination $ZCMConfigPath -Force
 				}
 
 			}
