fd38856a34aff974e240b2cb1ba7c7b858021b1b
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2026-08-24 01:50:45 -0400

    Refactor 10_PrepBISF_RES.ps1 to correct typos in documentation and improve variable naming consistency. This update enhances the clarity of the script's purpose and improves overall readability while preserving its functionality.

--- Framework/SubCall/Preparation/10_PrepBISF_RES.ps1
+++ Framework/SubCall/Preparation/10_PrepBISF_RES.ps1
@@ -1,13 +1,13 @@
 ﻿<#
 	.SYNOPSIS
-		Prepare RES One Workspace Management, RES ONE Automation and RES ONE Service Store Software for Image Managemement
+		Prepare RES One Workspace Management, RES ONE Automation and RES ONE Service Store Software for Image Management
 	.DESCRIPTION
 	  	Delete computer specific entries
 	.EXAMPLE
 	.NOTES
 		Author: Matthias Schlimm
 
-		Thanks to Company RES Germany: Oliver Lomberg & Nina Metz for additional enhacements informations to create this script
+		Thanks to Company RES Germany: Oliver Lomberg & Nina Metz for additional enhancements information to create this script
 
 		History:
 		10.01.2017 MS: Initial Script Created
@@ -15,20 +15,16 @@
 		30.01.2017 MS: RES Workspace Manager: add IF (Test-Path "$HKLM_WIN_CVN\WUID") {Remove-Item -Path "$HKLM_WIN_CVN\WUID"}
 		31.01.2017 MS: Added RES ONE Automation Console stop service
 		31.01.2017 MS: RES Workspace: change Remove-Item -Path "$InstallDir_REG\Data\DBCache\Resources\custom_resources\*" -recurse
-		01.02.2017 MS: Bugfix wrong syntax for RES ONE Automation Console
+		01.02.2017 MS: Fixed wrong syntax for RES ONE Automation Console
 		15.03.2017 MS: added Support for RES ONE Automation Agent Version 10 with new path in registry and filesystem
-		03.04.2017 MS: BugFix - RES Workspace: wrong Path in Workspace Agent, change from DBCache to LocalCachePath
-		03.04.2017 MS: BugFix - RES Workspace: delete not all folders in the CachePath
-		12.07.2017 FF: BugFix for Redirecting RES Cache (Setting Cache Path to WCD)
+		03.04.2017 MS: Fixed - RES Workspace: wrong Path in Workspace Agent, change from DBCache to LocalCachePath
+		03.04.2017 MS: Fixed - RES Workspace: delete not all folders in the CachePath
+		12.07.2017 FF: Fixed for Redirecting RES Cache (Setting Cache Path to WCD)
 		21.09.2017 MS: Feature: RES Automation Agent Service could be controlled from ADMX
-		04.05.2019 MS: BugFix 82 - RES ONE Automation Agent - Action is missing
-		14.08.2019 MS: FRQ 3 - Remove Messagebox and using default setting if GPO is not configured
-
+		04.05.2019 MS: Fixed 82 - RES ONE Automation Agent - Action is missing
+		14.08.2019 MS: FRQ 3 - Remove message box and using default setting if GPO is not configured
 		17.08.2019 MS: ENH 78: Sealing for Ivanti Automation agent can be disabled in ADMX
 		18.02.2020 JK: Fixed Log output spelling
-		
-	.LINK
-		https://eucweb.com
 #>
 
 Begin {
@@ -53,9 +49,9 @@ Begin {
 	$Prd4 = "RES ONE Service Store Client Service"
 	$Svc4 = "RESOCWSVC"
 
-	$HKLM_REG_ROW = "$HKLM_sw_x86\RES\Workspace Manager"
-	$HKLM_REG_ROA = @() # would be set in the RES ONE Automation Agent section, because of different path between RES Versions 9 and 10
-	$HKLM_WIN_CVN = "$HKLM_sw_x86\Microsoft\Windows\CurrentVersion"
+	$HklmRegRow = "$HklmSoftwareX86\RES\Workspace Manager"
+	$HklmRegRoa = @() # would be set in the RES ONE Automation Agent section, because of different path between RES Versions 9 and 10
+	$HklmWinCvn = "$HklmSoftwareX86\Microsoft\Windows\CurrentVersion"
 
 	####################################################################
 }
@@ -64,51 +60,51 @@ Process {
 
 
 	#RES ONE Workspace Agent
-	$svc = Test-BISFService -ServiceName $Svc1 -ProductName $Prd1
-	IF ($svc) {
+	$Svc = Test-BISFService -ServiceName $Svc1 -ProductName $Prd1
+	IF ($Svc) {
 		Invoke-BISFService -ServiceName $Svc1 -Action Stop
 		Write-BISFLog -Msg "Prepare for Imaging..."
 		Invoke-BISFService -ServiceName "RESPESVC" -Action Stop
 
-		$TestROWValue = Test-BISFRegistryValue -Path "$HKLM_REG_ROW" -Value "CachedSystemInfo"
-		IF ($TestROWValue) { Remove-ItemProperty -Path "$HKLM_REG_ROW" -Name "CachedSystemInfo" }
+		$TestROWValue = Test-BISFRegistryValue -Path "$HklmRegRow" -Value "CachedSystemInfo"
+		IF ($TestROWValue) { Remove-ItemProperty -Path "$HklmRegRow" -Name "CachedSystemInfo" }
 
-		$TestROWValue = Test-BISFRegistryValue -Path "$HKLM_REG_ROW" -Value "CachedSystemInfoEx"
-		IF ($TestROWValue) { Remove-ItemProperty -Path "$HKLM_REG_ROW" -Name "CachedSystemInfoEx" }
+		$TestROWValue = Test-BISFRegistryValue -Path "$HklmRegRow" -Value "CachedSystemInfoEx"
+		IF ($TestROWValue) { Remove-ItemProperty -Path "$HklmRegRow" -Name "CachedSystemInfoEx" }
 
-		$TestROWValue = Test-BISFRegistryValue -Path "$HKLM_REG_ROW" -Value "ComputerGUID"
-		IF ($TestROWValue) { Remove-ItemProperty -Path "$HKLM_REG_ROW" -Name "ComputerGUID" }
+		$TestROWValue = Test-BISFRegistryValue -Path "$HklmRegRow" -Value "ComputerGUID"
+		IF ($TestROWValue) { Remove-ItemProperty -Path "$HklmRegRow" -Name "ComputerGUID" }
 
-		$TestROWValue = Test-BISFRegistryValue -Path "$HKLM_REG_ROW" -Value "LastSyncUTC"
-		IF ($TestROWValue) { Remove-ItemProperty -Path "$HKLM_REG_ROW" -Name "LastSyncUTC" }
+		$TestROWValue = Test-BISFRegistryValue -Path "$HklmRegRow" -Value "LastSyncUTC"
+		IF ($TestROWValue) { Remove-ItemProperty -Path "$HklmRegRow" -Name "LastSyncUTC" }
 
-		IF (Test-Path "$HKLM_REG_ROW\UpdateGUIDs") { Remove-Item -Path "$HKLM_REG_ROW\UpdateGUIDs" }
+		IF (Test-Path "$HklmRegRow\UpdateGUIDs") { Remove-Item -Path "$HklmRegRow\UpdateGUIDs" }
 
-		IF (Test-Path "$HKLM_WIN_CVN\WUID") { Remove-Item -Path "$HKLM_WIN_CVN\WUID" }
+		IF (Test-Path "$HklmWinCvn\WUID") { Remove-Item -Path "$HklmWinCvn\WUID" }
 
-		$TestROWValue = Test-BISFRegistryValue -Path "$HKLM_REG_ROW" -Value "LocalCachePath"
+		$TestROWValue = Test-BISFRegistryValue -Path "$HklmRegRow" -Value "LocalCachePath"
 		IF ($TestROWValue) {
-			$LocalCachePath_REG = Get-ItemProperty -path "$HKLM_REG_ROW" | % { $_.LocalCachePath }
-			Write-BISFLog -Msg "LocalCachePath is set to $LocalCachePath_REG and would deleted now"
-			IF (Test-Path $LocalCachePath_REG) { Remove-Item -Path "$LocalCachePath_REG" -recurse -force }
+			$LocalCachePathReg = Get-ItemProperty -path "$HklmRegRow" | ForEach-Object { $_.LocalCachePath }
+			Write-BISFLog -Msg "LocalCachePath is set to $LocalCachePathReg and would deleted now"
+			IF (Test-Path $LocalCachePathReg) { Remove-Item -Path "$LocalCachePathReg" -recurse -force }
 		}
 		ELSE {
-			$InstallDir_REG = Get-ItemProperty -path "$HKLM_REG_ROW" | % { $_.InstallDir }
+			$InstallDirReg = Get-ItemProperty -path "$HklmRegRow" | ForEach-Object { $_.InstallDir }
 
-			IF (Test-Path $InstallDir_REG) {
-				Write-BISFLog -Msg "DB Cache is set to $InstallDir_REG\Data\DBCache and will be deleted now"
-				Remove-Item -Path "$InstallDir_REG\Data\DBCache" -recurse
+			IF (Test-Path $InstallDirReg) {
+				Write-BISFLog -Msg "DB Cache is set to $InstallDirReg\Data\DBCache and will be deleted now"
+				Remove-Item -Path "$InstallDirReg\Data\DBCache" -recurse
 			}
 			ELSE {
-				Write-BISFLog -Msg "DB Cache is set to $InstallDir_REG\Data\DBCache and could NOT be deleted" -Type W -SubMsg
+				Write-BISFLog -Msg "DB Cache is set to $InstallDirReg\Data\DBCache and could NOT be deleted" -Type W -SubMsg
 
 			}
 		}
 
-		IF ($returnTestPVSSoftware -eq "true") {
+		IF ($ReturnTestPVSSoftware -eq "true") {
 
 			Write-BISFLog -Msg "Citrix PVS Target Device Driver installed" -SubMsg -ShowConsole -Color DarkCyan
-			$ROWCachePath = (Get-ItemProperty "$HKLM_REG_ROW").DBCache
+			$ROWCachePath = (Get-ItemProperty "$HklmRegRow").DBCache
 			$ROWCachePathDrive = $ROWCachePath.substring(0, 2)
 			IF ($PVSDiskDrive -eq $ROWCachePathDrive) {
 				Write-BISFLog -Msg "RES Workspace Manager DBCache is already redirected $ROWCachePath" -SubMsg -ShowConsole -Color DarkCyan
@@ -116,66 +112,66 @@ Process {
 			ELSE {
 				$ROWCachePath = "$PVSDiskDrive\RES\Workspace Manager\DBCache"
 				Write-BISFLog -Msg "Redirecting RES ONE Workspace Agent DBCache to $ROWCachePath" -SubMsg -ShowConsole -Color DarkCyan
-				Set-ItemProperty -Path "$HKLM_REG_ROW" -Name "LocalCachePath" -Value "$ROWCachePath"
-				Set-ItemProperty -Path "$HKLM_REG_ROW" -Name "LocalCacheOnDisk" -Value "YES"
+				Set-ItemProperty -Path "$HklmRegRow" -Name "LocalCachePath" -Value "$ROWCachePath"
+				Set-ItemProperty -Path "$HklmRegRow" -Name "LocalCacheOnDisk" -Value "YES"
 			}
 		}
 	}
 
 	#RES ONE Automation Console
-	$svc = Test-BISFService -ServiceName $Svc2 -ProductName $Prd2
-	IF ($svc) {
+	$Svc = Test-BISFService -ServiceName $Svc2 -ProductName $Prd2
+	IF ($Svc) {
 		Invoke-BISFService -ServiceName $Svc2 -Action Stop
 	}
 
 	#RES ONE Automation
-	$svc = Test-BISFService -ServiceName $Svc3 -ProductName $Prd3
-	IF ($svc) {
+	$Svc = Test-BISFService -ServiceName $Svc3 -ProductName $Prd3
+	IF ($Svc) {
 		IF ($LIC_BISF_CLI_RA_SVC -eq "YES") { Invoke-BISFService -ServiceName $Svc3 -Action Stop } ELSE { Write-BISFLog -Msg "$Prd3 Service would not stopped (ADMX configuration)" -SubMsg -ShowConsole -Color DarkCyan }
 		IF ($LIC_BISF_CLI_RA_SEAL -ne "YES") {
 			Write-BISFLog -Msg "Prepare $Prd3 for Imaging..."
-			$glbSVCImagePath = $glbSVCImagePath.split("\")[1] #get $SVCImagePath from Test-BISFService and split them to get ProgramFiles or ProgramFiles(x86) only
-			IF ($glbSVCImagePath -eq "Program Files") {
-				$HKLM_REG_ROA = "$hklm_software\RES\AutomationManager"
-				$Inst_Path_ROA = "C:\$glbSVCImagePath\RES\Automation\Agent"
+			$GlbSVCImagePath = $GlbSVCImagePath.split("\")[1] #get $SVCImagePath from Test-BISFService and split them to get ProgramFiles or ProgramFiles(x86) only
+			IF ($GlbSVCImagePath -eq "Program Files") {
+				$HklmRegRoa = "$HklmSoftware\RES\AutomationManager"
+				$InstPathROA = "C:\$GlbSVCImagePath\RES\Automation\Agent"
 			}
 			ELSE {
-				$HKLM_REG_ROA = "$HKLM_sw_x86\RES\AutomationManager"
-				$Inst_Path_ROA = "C:\$glbSVCImagePath\RES Software\Automation Manager\Agent"
+				$HklmRegRoa = "$HklmSoftwareX86\RES\AutomationManager"
+				$InstPathROA = "C:\$GlbSVCImagePath\RES Software\Automation Manager\Agent"
 			}
-			Write-BISFLog -Msg "RES ROA Registry Path: $HKLM_REG_ROA"
-			Write-BISFLog -Msg "RES ROA Install Path: $Inst_Path_ROA"
+			Write-BISFLog -Msg "RES ROA Registry Path: $HklmRegRoa"
+			Write-BISFLog -Msg "RES ROA Install Path: $InstPathROA"
 
-			$TestROAValue = Test-BISFRegistryValue -Path "$HKLM_REG_ROA\Agent" -Value "CachedDispatchers"
-			IF ($TestROAValue) { Remove-ItemProperty -Path "$HKLM_REG_ROA\Agent" -Name "CachedDispatchers" }
+			$TestROAValue = Test-BISFRegistryValue -Path "$HklmRegRoa\Agent" -Value "CachedDispatchers"
+			IF ($TestROAValue) { Remove-ItemProperty -Path "$HklmRegRoa\Agent" -Name "CachedDispatchers" }
 
-			$TestROAValue = Test-BISFRegistryValue -Path "$HKLM_REG_ROA\Agent" -Value "CommunicationID"
-			IF ($TestROAValue) { Remove-ItemProperty -Path "$HKLM_REG_ROA\Agent" -Name "CommunicationID" }
+			$TestROAValue = Test-BISFRegistryValue -Path "$HklmRegRoa\Agent" -Value "CommunicationID"
+			IF ($TestROAValue) { Remove-ItemProperty -Path "$HklmRegRoa\Agent" -Name "CommunicationID" }
 
-			$TestROAValue = Test-BISFRegistryValue -Path "$HKLM_REG_ROA\Agent" -Value "DispatcherListKGC"
-			IF ($TestROAValue) { Remove-ItemProperty -Path "$HKLM_REG_ROA\Agent" -Name "DispatcherListKGC" }
+			$TestROAValue = Test-BISFRegistryValue -Path "$HklmRegRoa\Agent" -Value "DispatcherListKGC"
+			IF ($TestROAValue) { Remove-ItemProperty -Path "$HklmRegRoa\Agent" -Name "DispatcherListKGC" }
 
-			Set-ItemProperty -Path "$HKLM_REG_ROA\Agent" -Name "Prepared4Image" -Value "BISFStaging"
+			Set-ItemProperty -Path "$HklmRegRoa\Agent" -Name "Prepared4Image" -Value "BISFStaging"
 
-			$TestROAValue = Test-BISFRegistryValue -Path "$HKLM_REG_ROA\Preferences" -Value "WUID"
-			IF ($TestROAValue) { Remove-ItemProperty -Path "$HKLM_REG_ROA\Preferences" -Name "WUID" }
+			$TestROAValue = Test-BISFRegistryValue -Path "$HklmRegRoa\Preferences" -Value "WUID"
+			IF ($TestROAValue) { Remove-ItemProperty -Path "$HklmRegRoa\Preferences" -Name "WUID" }
 
-			IF (Test-Path "$HKLM_WIN_CVN\WUID") { Remove-Item -Path "$HKLM_WIN_CVN\WUID" }
+			IF (Test-Path "$HklmWinCvn\WUID") { Remove-Item -Path "$HklmWinCvn\WUID" }
 
-			$TestROAValue = Test-BISFRegistryValue -Path "$HKLM_REG_ROA\Agent" -Value "LastKnownResourceCacheFolder"
+			$TestROAValue = Test-BISFRegistryValue -Path "$HklmRegRoa\Agent" -Value "LastKnownResourceCacheFolder"
 			IF ($TestROAValue) {
-				$LastKnownResourceCacheFolder_REG = Get-ItemProperty -path "$HKLM_REG_ROA" | % { $_.LastKnownResourceCacheFolder }
-				Write-BISFLog -Msg "LastKnownResourceCacheFolder is set to $LastKnownResourceCacheFolder_REG and would deleted now"
-				IF (Test-Path $LastKnownResourceCacheFolder_REG) { Remove-Item -Path "$LastKnownResourceCacheFolder_REG\*" -recurse }
+				$LastKnownResourceCacheFolderReg = Get-ItemProperty -path "$HklmRegRoa" | ForEach-Object { $_.LastKnownResourceCacheFolder }
+				Write-BISFLog -Msg "LastKnownResourceCacheFolder is set to $LastKnownResourceCacheFolderReg and would deleted now"
+				IF (Test-Path $LastKnownResourceCacheFolderReg) { Remove-Item -Path "$LastKnownResourceCacheFolderReg\*" -recurse }
 			}
 			ELSE {
-				$ROA_StdPath = "$Inst_Path_ROA\Workspace"
-				IF (Test-Path $ROA_StdPath) {
-					Write-BISFLog -Msg "Standardpath is set to $ROA_StdPath and will be deleted now"
-					Remove-Item -Path "$ROA_StdPath\*" -recurse
+				$ROAStdPath = "$InstPathROA\Workspace"
+				IF (Test-Path $ROAStdPath) {
+					Write-BISFLog -Msg "Standard path is set to $ROAStdPath and will be deleted now"
+					Remove-Item -Path "$ROAStdPath\*" -recurse
 				}
 				ELSE {
-					Write-BISFLog -Msg "Standardpath is set to $ROA_StdPath and could NOT be deleted" -Type W -SubMsg
+					Write-BISFLog -Msg "Standard path is set to $ROAStdPath and could NOT be deleted" -Type W -SubMsg
 				}
 			}
 		}
@@ -183,9 +179,9 @@ Process {
 			Write-BISFLog -Msg "Sealing for $Prd3 is skipped from GPO configuration" -Type W -SubMsg
 		}
 
-		IF ($returnTestPVSSoftware -eq "true") {
+		IF ($ReturnTestPVSSoftware -eq "true") {
 			Write-BISFLog -Msg "Citrix PVS Target Device Driver installed" -SubMsg -ShowConsole -Color DarkCyan
-			$ROACachePath = (Get-ItemProperty "$HKLM_REG_ROA\Agent").LastKnownResourceCacheFolder
+			$ROACachePath = (Get-ItemProperty "$HklmRegRoa\Agent").LastKnownResourceCacheFolder
 			$ROACachePathDrive = $ROACachePath.substring(0, 2)
 			IF ($PVSDiskDrive -eq $ROACachePathDrive) {
 				Write-BISFLog -Msg "RES Automation Manager DBCache is already redirected to $ROACachePath" -SubMsg -ShowConsole -Color DarkCyan
@@ -193,33 +189,33 @@ Process {
 			ELSE {
 				$ROACachePath = "$PVSDiskDrive\RES\Automation Manager\DBCache"
 				Write-BISFLog -Msg "Redirect RES ONE Automation Manager Agent DBCache to $ROACachePath" -SubMsg -ShowConsole -Color DarkCyan
-				Set-ItemProperty -Path "$HKLM_REG_ROA\Agent" -Name "LastKnownResourceCacheFolder" -Value "$ROACachePath"
+				Set-ItemProperty -Path "$HklmRegRoa\Agent" -Name "LastKnownResourceCacheFolder" -Value "$ROACachePath"
 			}
 		}
 
 		Write-BISFLog -Msg "Check GPO Configuration" -SubMsg -Color DarkCyan
-		$varCLI = $LIC_BISF_CLI_RA
-		IF (($varCLI -eq "YES") -or ($varCLI -eq "NO")) {
-			Write-BISFLog -Msg "GPO Valuedata: $varCLI"
+		$VarCLI = $LIC_BISF_CLI_RA
+		IF (($VarCLI -eq "YES") -or ($VarCLI -eq "NO")) {
+			Write-BISFLog -Msg "GPO value:: $VarCLI"
 		}
 		ELSE {
 			Write-BISFLog -Msg "GPO not configured.. using default setting" -SubMsg -Color DarkCyan
 			$ROADisableSVC = "NO"
 		}
 
-		If (($ROADisableSVC -eq "YES" ) -or ($varCLI -eq "YES")) {
+		If (($ROADisableSVC -eq "YES" ) -or ($VarCLI -eq "YES")) {
 			Write-BISFLog -Msg "reconfigure Service... please Wait"
 			Invoke-BISFService -ServiceName "$Svc3" -StartType Disabled -Action Stop
 
 		}
 		ELSE {
-			Write-BISFLog -Msg "Sealing for $Prd3 is disabled in GPO" -ShowConsole -SubMSg -Type W
+			Write-BISFLog -Msg "Sealing for $Prd3 is disabled in GPO" -ShowConsole -SubMsg -Type W
 		}
 	}
 
 	#RES ONE Service Store Client Service
-	$svc = Test-BISFService -ServiceName $Svc4 -ProductName $Prd4
-	IF ($svc) {
+	$Svc = Test-BISFService -ServiceName $Svc4 -ProductName $Prd4
+	IF ($Svc) {
 		Invoke-BISFService -ServiceName $Svc4 -Action Stop
 	}
 
