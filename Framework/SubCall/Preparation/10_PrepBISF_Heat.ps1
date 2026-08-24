6df747d96a9ab913cd09df46c9d2c7ab32fe71e9
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2026-08-24 01:51:05 -0400

    Refactor 10_PrepBISF_Heat.ps1 to correct typos in documentation and improve variable naming consistency. This update enhances clarity and readability while maintaining the script's functionality.

--- Framework/SubCall/Preparation/10_PrepBISF_Heat.ps1
+++ Framework/SubCall/Preparation/10_PrepBISF_Heat.ps1
@@ -6,40 +6,36 @@
 	.EXAMPLE
 	.NOTES
 		Author: Matthias Schlimm
-		Company:  EUCWeb.com
 
 		History:
 		17.02.2015 MS: Script created
 		30.09.2015 MS: Rewritten script with standard .SYNOPSIS, use central BISF function to configure service
 		04.11.2015 MS: Syntax error -> replace WriteBISF-Log with Write-BISFLog
-		10.12.2015 MS: Change Productname from "Frantrange DSM " to "Heat DSM"
+		10.12.2015 MS: Change product name from "Frontrange DSM " to "Heat DSM"
 		18.02.2020 JK: Fixed Log output spelling
-
-	.LINK
-		https://eucweb.com
 #>
 
 Begin {
-	$script_path = $MyInvocation.MyCommand.Path
-	$script_dir = Split-Path -Parent $script_path
-	$script_name = [System.IO.Path]::GetFileName($script_path)
+	$ScriptPath = $MyInvocation.MyCommand.Path
+	$ScriptDir = Split-Path -Parent $ScriptPath
+	$ScriptName = [System.IO.Path]::GetFileName($ScriptPath)
 	$Product = "Heat DSM"
-	$product_path = "$ProgramFilesx86\NetInst"
+	$ProductPath = "$ProgramFilesx86\NetInst"
 	$PrepApp = "niprep.exe"
-	$servicename = "esiCore"
+	$ServiceName = "esiCore"
 }
 
 Process {
 
-	$svc = Test-BISFService -ServiceName "$servicename" -ProductName "$product"
-	IF ($svc -eq $true) {
+	$Svc = Test-BISFService -ServiceName "$ServiceName" -ProductName "$Product"
+	IF ($Svc -eq $true) {
 		Write-BISFLog -Msg "Preparing $Product for Imaging" -ShowConsole -Color DarkCyan -SubMsg
-		IF (Test-Path ("$product_path\$PrepApp") -PathType Leaf ) {
+		IF (Test-Path ("$ProductPath\$PrepApp") -PathType Leaf ) {
 			Write-BISFLog -Msg "Preparing $Product for Imaging "
-			& Start-Process -FilePath "$product_path\$PrepApp" -ArgumentList "/r" -Wait
+			& Start-Process -FilePath "$ProductPath\$PrepApp" -ArgumentList "/r" -Wait
 		}
 		ELSE {
-			Write-BISFLog -Msg "$product_path\$PrepApp does not exists. Image Preperation could not be performed!" -Type E -SubMsg
+			Write-BISFLog -Msg "$ProductPath\$PrepApp does not exists. Image Preparation could not be performed!" -Type E -SubMsg
 		}
 
 	}
