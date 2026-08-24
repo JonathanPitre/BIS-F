c593b8c3e7d97c3a9aa3ba7c34bd3ad29aca40bb
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2026-08-24 02:02:50 -0400

    Refactor 10_PrepBISF_AV-EPC.ps1 to improve variable naming consistency and enhance documentation clarity. This update includes corrections to variable names and adjustments to the history section, maintaining the script's functionality while improving readability.

--- Framework/SubCall/Preparation/10_PrepBISF_AV-EPC.ps1
+++ Framework/SubCall/Preparation/10_PrepBISF_AV-EPC.ps1
@@ -6,52 +6,49 @@
 	.EXAMPLE
 	.NOTES
 		Author: Matthias Schlimm
-	  	Company:  EUCWeb.com
 
 		History:
 	  	25.03.2014 MS: Script created
 		01.04.2014 MS: Changed Console message
-		12.05.2014 MS: Changed Fullscan from Windows Defender directory to '$MSC_path\...'
-		13.05.2014 MS: Added Silentswitch -AVFullScan (YES|NO)
-		11.06.2014 MS: Syntax error to start silent pattern update and fullscan, fix read variable LIC_BISF_CLI_AV
-		13.08.2014 MS: Removed $logfile = Set-logFile, it would be used in the 10_XX_LIB_Config.ps1 Script only
-		20.02.2015 MS: Added progressbar during fullscan
+		12.05.2014 MS: Changed full scan from Windows Defender directory to '$MSC_path\...'
+		13.05.2014 MS: Added Silent switch -AVFullScan (YES|NO)
+		11.06.2014 MS: Syntax error to start silent pattern update and full scan, fix read variable LIC_BISF_CLI_AV
+		13.08.2014 MS: Removed $LogFile = Set-LogFile, it would be used in the 10_XX_LIB_Config.ps1 Script only
+		20.02.2015 MS: Added progress bar during full scan
 		30.09.2015 MS: Rewritten script with standard .SYNOPSIS, use central BISF function to configure service
-		06.03.2017 MS: Bugfix read Variable $varCLI = ...
-		16.08.2019 MS: FRQ 3 - Remove Messagebox and using default setting if GPO is not configured
-		03.10.2019 MS: ENH 51 - ADMX Extension: select AnitVirus full scan or custom Scan arguments
+		06.03.2017 MS: Fixed read Variable $varCLI = ...
+		16.08.2019 MS: FRQ 3 - Remove message box and using default setting if GPO is not configured
+		03.10.2019 MS: ENH 51 - ADMX Extension: select AntiVirus full scan or custom Scan arguments
 		18.02.2020 JK: Fixed Log output spelling
-	.LINK
-		https://eucweb.com
 #>
 
 Begin {
 	$PSScriptFullName = $MyInvocation.MyCommand.Path
 	$PSScriptRoot = Split-Path -Parent $PSScriptFullName
 	$PSScriptName = [System.IO.Path]::GetFileName($PSScriptFullName)
-	$product = "Microsoft Security Client"
-	$MSC_path = "C:\Program Files\$product"
+	$Product = "Microsoft Security Client"
+	$MSCPath = "C:\Program Files\$Product"
 }
 
 Process {
 
-	function MSCrun {
+	function Invoke-SecurityClientScan {
 
 		Write-BISFLog -Msg "Update VirusSignatures"
-		& "$MSC_path\MpCMDrun.exe" -SignatureUpdate
+		& "$MSCPath\MpCMDrun.exe" -SignatureUpdate
 
 		Write-BISFLog -Msg "Check GPO Configuration" -SubMsg -Color DarkCyan
-		$varCLI = $LIC_BISF_CLI_AV
+		$VarCLI = $LIC_BISF_CLI_AV
 
-		IF (($varCLI -eq "YES") -or ($varCLI -eq "NO")) {
-			Write-BISFLog -Msg "GPO Valuedata: $varCLI"
+		IF (($VarCLI -eq "YES") -or ($VarCLI -eq "NO")) {
+			Write-BISFLog -Msg "GPO value:: $VarCLI"
 		}
 		ELSE {
 			Write-BISFLog -Msg "GPO not configured.. using default setting" -SubMsg -Color DarkCyan
 			$AVScan = "YES"
 		}
 
-		if (($AVScan -eq "YES" ) -or ($varCLI -eq "YES")) {
+		if (($AVScan -eq "YES" ) -or ($VarCLI -eq "YES")) {
 			IF ($LIC_BISF_CLI_AV_VIE_CusScanArgsb -eq 1) {
 				Write-BISFLog -Msg "Enable Custom Scan Arguments"
 				$args = $LIC_BISF_CLI_AV_VIE_CusScanArgs
@@ -61,8 +58,8 @@ Process {
 			}
 
 			Write-BISFLog -Msg "Running Scan with arguments: $args"
-			Start-Process -FilePath "$MSC_path\MpCMDrun.exe" -ArgumentList $args
-			Show-ProgressBar -CheckProcess "MpCMDrun" -ActivityText "$Product is scanning the system...please wait"
+			Start-Process -FilePath "$MSCPath\MpCMDrun.exe" -ArgumentList $args
+			Show-BISFProgressBar -CheckProcess "MpCMDrun" -ActivityText "$Product is scanning the system...please wait"
 		}
 		ELSE {
 			Write-BISFLog -Msg "No Scan will be performed"
@@ -74,9 +71,9 @@ Process {
 	####################################################################
 
 	#### Main Program
-	IF (Test-Path ("$MSC_path\MpCMDRun.exe") -PathType Leaf ) {
+	IF (Test-Path ("$MSCPath\MpCMDRun.exe") -PathType Leaf ) {
 		Write-BISFLog -Msg "$Product installed" -ShowConsole -Color Cyan
-		MSCrun
+		Invoke-SecurityClientScan
 	}
 	ELSE {
 		Write-BISFLog -Msg "$Product is not installed"
