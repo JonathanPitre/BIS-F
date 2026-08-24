a947556fc31f80d10681eda15fd0cc8851a80e01
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2026-08-24 02:00:06 -0400

    Refactor 10_PrepBISF_AV-F-Secure.ps1 to correct typos in documentation and improve variable naming consistency. This update enhances clarity in the script's purpose and improves overall readability while maintaining its functionality.

--- Framework/SubCall/Preparation/10_PrepBISF_AV-F-Secure.ps1
+++ Framework/SubCall/Preparation/10_PrepBISF_AV-F-Secure.ps1
@@ -1,6 +1,6 @@
 ﻿<#
     .SYNOPSIS
-        Prepare F-Secure AntiVirus for Image Managemement
+        Prepare F-Secure AntiVirus for Image Management
 	.DESCRIPTION
       	Scan system and stop services
     .EXAMPLE
@@ -9,22 +9,19 @@
 
 		History:
 		  29.07.2017 MS: Script created
-		  14.08.2019 MS: FRQ 3 - Remove Messagebox and using default setting if GPO is not configured
-			03.10.2019 MS: ENH 51 - ADMX Extension: select AnitVirus full scan or custom Scan arguments
+		  14.08.2019 MS: FRQ 3 - Remove message box and using default setting if GPO is not configured
+			03.10.2019 MS: ENH 51 - ADMX Extension: select AntiVirus full scan or custom Scan arguments
 			18.02.2020 JK: Fixed Log output spelling
-
-	.LINK
-        https://eucweb.com
 #>
 
 Begin {
-	$script_path = $MyInvocation.MyCommand.Path
-	$script_dir = Split-Path -Parent $script_path
-	$script_name = [System.IO.Path]::GetFileName($script_path)
+	$ScriptPath = $MyInvocation.MyCommand.Path
+	$ScriptDir = Split-Path -Parent $ScriptPath
+	$ScriptName = [System.IO.Path]::GetFileName($ScriptPath)
 
 	# Product specified
 	$Product = "F-Secure Anti-Virus"
-	$Inst_path = "$ProgramFilesx86\F-Secure\Anti-Virus"
+	$InstPath = "$ProgramFilesx86\F-Secure\Anti-Virus"
 	$ServiceNames = @("FSAUA", "FSMA", "F-Secure Network Request Broker", "FSORSPClient", "F-Secure WebUI Daemon", "F-Secure Gatekeeper Handler Starter")
 }
 
@@ -38,15 +35,15 @@ Process {
 	function RunFullScan {
 
 		Write-BISFLog -Msg "Check GPO Configuration" -SubMsg -Color DarkCyan
-		$varCLI = $LIC_BISF_CLI_AV
-		IF (($varCLI -eq "YES") -or ($varCLI -eq "NO")) {
-			Write-BISFLog -Msg "GPO Valuedata: $varCLI"
+		$VarCLI = $LIC_BISF_CLI_AV
+		IF (($VarCLI -eq "YES") -or ($VarCLI -eq "NO")) {
+			Write-BISFLog -Msg "GPO value:: $VarCLI"
 		}
 		ELSE {
 			Write-BISFLog -Msg "GPO not configured.. using default setting" -SubMsg
 			$AVScan = "YES"
 		}
-		if (($AVScan -eq "YES" ) -or ($varCLI -eq "YES")) {
+		if (($AVScan -eq "YES" ) -or ($VarCLI -eq "YES")) {
 			IF ($LIC_BISF_CLI_AV_VIE_CusScanArgsb -eq 1) {
 				Write-BISFLog -Msg "Enable Custom Scan Arguments"
 				$args = $LIC_BISF_CLI_AV_VIE_CusScanArgs
@@ -56,7 +53,7 @@ Process {
 			}
 
 			Write-BISFLog -Msg "Running Scan with arguments: $args"
-			Start-Process -FilePath "$Inst_path\fsav.exe" -ArgumentList $args
+			Start-Process -FilePath "$InstPath\fsav.exe" -ArgumentList $args
 			Show-BISFProgressBar -CheckProcess "$ScanProcess" -ActivityText "$Product is scanning the system...please wait"
 			IF (Test-Path "C:\Windows\Logs\fsavlog.txt") {
 				Get-BISFLogContent -GetLogFile "C:\Windows\Logs\fsavlog.txt"
@@ -73,8 +70,8 @@ Process {
 
 	function StopService {
 		ForEach ($ServiceName in $ServiceNames) {
-			$svc = Test-BISFService -ServiceName "$ServiceName"
-			IF ($svc -eq $true) { Invoke-BISFService -ServiceName "$($ServiceName)" -Action Stop }
+			$Svc = Test-BISFService -ServiceName "$ServiceName"
+			IF ($Svc -eq $true) { Invoke-BISFService -ServiceName "$($ServiceName)" -Action Stop }
 		}
 	}
 
@@ -83,8 +80,8 @@ Process {
 	####################################################################
 
 	#### Main Program
-	$svc = Test-BISFService -ServiceName $ServiceNames[1] -ProductName "$product"
-	IF ($svc -eq $true) {
+	$Svc = Test-BISFService -ServiceName $ServiceNames[1] -ProductName "$Product"
+	IF ($Svc -eq $true) {
 		RunFullScan
 		StopService
 	}
