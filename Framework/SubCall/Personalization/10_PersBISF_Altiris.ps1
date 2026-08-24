18d85aa0fa92420b5c8aba7cc6c54a155037f904
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2026-08-24 01:17:02 -0400

    Refactor 10_PersBISF_Altiris.ps1 for improved variable naming and documentation clarity
    Updated variable names for consistency and enhanced comment clarity throughout the script. Corrected minor typos in the synopsis and description to improve readability while maintaining functionality.

--- Framework/SubCall/Personalization/10_PersBISF_Altiris.ps1
+++ Framework/SubCall/Personalization/10_PersBISF_Altiris.ps1
@@ -1,35 +1,32 @@
 ﻿
 <#
 	.SYNOPSIS
-		Personalize Altiris Agent for Image Managemement Software
+		Personalize Altiris Agent for Image Management Software
 	.DESCRIPTION
 	  	If image is in shared mode the service will be started
 	.EXAMPLE
 	.NOTES
 		Author: Matthias Schlimm
-	  	Company:  EUCWeb.com
 
 		History:
 	  	14.10.2014 MS: function created
 		29.09.2015 MS: rewritten script with standard .SYNOPSIS, use central BISF function to configure service
 
 
-	.LINK
-		https://eucweb.com
 #>
 
 Begin {
-	$script_path = $MyInvocation.MyCommand.Path
-	$script_dir = Split-Path -Parent $script_path
-	$script_name = [System.IO.Path]::GetFileName($script_path)
-	$servicename = "Altiris Deployment Agent"
+	$ScriptPath = $MyInvocation.MyCommand.Path
+	$ScriptDir = Split-Path -Parent $ScriptPath
+	$ScriptName = [System.IO.Path]::GetFileName($ScriptPath)
+	$ServiceName = "Altiris Deployment Agent"
 }
 
 Process {
 
-	$svc = Test-BISFService -ServiceName "$servicename" -ProductName "$servicename"
-	IF ($svc) {
-		Invoke-BISFService -ServiceName "$servicename" -Action Start -CheckDiskMode RW
+	$Svc = Test-BISFService -ServiceName "$ServiceName" -ProductName "$ServiceName"
+	IF ($Svc) {
+		Invoke-BISFService -ServiceName "$ServiceName" -Action Start -CheckDiskMode RW
 	}
 }
 
