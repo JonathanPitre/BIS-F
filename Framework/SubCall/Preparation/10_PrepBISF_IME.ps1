844933a9d703e73eb43968770eb915d9f8cd3821
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2026-08-24 02:01:15 -0400

    Refactor 10_PrepBISF_IME.ps1 to improve variable naming consistency and update documentation. This change enhances clarity in the script's purpose and improves overall readability while maintaining its functionality.

--- Framework/SubCall/Preparation/10_PrepBISF_IME.ps1
+++ Framework/SubCall/Preparation/10_PrepBISF_IME.ps1
@@ -3,18 +3,14 @@ param(
 )
 <#
 	.SYNOPSIS
-		Delete Office 2010 IME Keyboards from Autorun
+		Delete Office 2010 IME Keyboards from startup
 	.DESCRIPTION
 	.EXAMPLE
 	.NOTES
 		Author: Benjamin Ruoff
-		Company:  EUCWeb.com
 
 		History
 		26.10.2015 MS: Script created
-
-	.LINK
-		https://eucweb.com
 #>
 
 Begin {
@@ -22,29 +18,29 @@ Begin {
 	####################################################################
 	# define environment
 
-	$script_path = $MyInvocation.MyCommand.Path
-	$script_dir = Split-Path -Parent $script_path
-	$script_name = [System.IO.Path]::GetFileName($script_path)
+	$ScriptPath = $MyInvocation.MyCommand.Path
+	$ScriptDir = Split-Path -Parent $ScriptPath
+	$ScriptName = [System.IO.Path]::GetFileName($ScriptPath)
 
 	# Product specified
 	$Product = "Office IME Languages Clean-up"
-	[array]$reg_IME_string = "$hklm_software\Microsoft\Windows\CurrentVersion\Run"
-	[array]$reg_IME_string += "$hklm_software\Wow6432Node\Microsoft\Windows\CurrentVersion\Run"
+	[array]$RegIMEString = "$HklmSoftware\Microsoft\Windows\CurrentVersion\Run"
+	[array]$RegIMEString += "$HklmSoftware\Wow6432Node\Microsoft\Windows\CurrentVersion\Run"
 
-	[array]$reg_IME_name = "IME14 JPN Setup"
-	[array]$reg_IME_name += "IME14 KOR Setup"
-	[array]$reg_IME_name += "IME14 CHS Setup"
-	[array]$reg_IME_name += "IME14 CHT Setup"
+	[array]$RegIMEName = "IME14 JPN Setup"
+	[array]$RegIMEName += "IME14 KOR Setup"
+	[array]$RegIMEName += "IME14 CHS Setup"
+	[array]$RegIMEName += "IME14 CHT Setup"
 
 	####################################################################
 
 	function deleteOfficeIME {
 		# Delete specified Data
-		foreach ($path in $reg_IME_string) {
-			foreach ($key in $reg_IME_name) {
-				Write-BISFLog -Msg "delete specified registry items in $($path)..."
-				Write-BISFLog -Msg "delete $key"
-				Remove-ItemProperty -Path $path -Name $key -ErrorAction SilentlyContinue
+		foreach ($Path in $RegIMEString) {
+			foreach ($Key in $RegIMEName) {
+				Write-BISFLog -Msg "delete specified registry items in $($Path)..."
+				Write-BISFLog -Msg "delete $Key"
+				Remove-ItemProperty -Path $Path -Name $Key -ErrorAction SilentlyContinue
 			}
 
 		}
