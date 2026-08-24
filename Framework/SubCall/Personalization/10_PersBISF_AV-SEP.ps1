300deca8bf772f2f8ebbce8a7ed0e451edfad374
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2026-08-24 01:17:29 -0400

    Correct typos and improve variable naming in 10_PersBISF_AV-SEP.ps1
    This update includes corrections to spelling errors in the synopsis and comments, as well as enhancements to variable naming for consistency. The changes aim to improve the overall readability and maintainability of the script while preserving its functionality.

--- Framework/SubCall/Personalization/10_PersBISF_AV-SEP.ps1
+++ Framework/SubCall/Personalization/10_PersBISF_AV-SEP.ps1
@@ -1,16 +1,15 @@
 ﻿<#
 	.SYNOPSIS
-		Prepapre Symantec Endpoint Protection for Image Managemement Software
+		Prepare Symantec Endpoint Protection for Image Management Software
 	.DESCRIPTION
 	  	Create HostID based on MACAddress
 	.EXAMPLE
 	.NOTES
 		Author: Matthias Schlimm
-		Company:  EUCWeb.com
 
 		History:
 		24.09.2012 MS: Script created
-		18.09.2013 MS: replace $date with $(Get-date) to get current timestamp at running scriptlines write to the logfile
+		18.09.2013 MS: replace $date with $(Get-date) to get current timestamp at running script lines write to the log file
 		18.09.2013 MS: replace $PVsWriteCacheDisk to global environment variable $LIC_PVS_HostIDPath
 		17.12.2013 MS: change service to smc -start for SEP 12 or higher
 		27.01.2014 MS: $HostID_Prfx = "00000000000000000000"
@@ -20,19 +19,17 @@
 		10.03.2014 MS: Review Code
 		11.03.2014 MS: IF (Test-Path ("$SEP_path\smc.exe"))
 		18.03.2014 BR: revisited Script
-		13.08.2014 MS: remove $logfile = Set-logFile, it would be used in the 10_XX_LIB_Config.ps1 Script only
+		13.08.2014 MS: remove $LogFile = Set-LogFile, it would be used in the 10_XX_LIB_Config.ps1 Script only
 		17.08.2014 MS: change line 32 to $SEP_path = "$ProgramFilesx86\Symantec\Symantec Endpoint Protection"
-		31.08.2015 MS: bugfix 89 - symantec fixes the registry location for the SEP-Client to WOW6432Node, fix in line 31-32 and function SetHostID
-		01.09.2015 MS: Bugfix 89 sucessfull tested
+		31.08.2015 MS: Fixed 89 - symantec fixes the registry location for the SEP-Client to WOW6432Node, fix in line 31-32 and function SetHostID
+		01.09.2015 MS: Fixed 89 successful tested
 		06.10.2015 MS: rewritten script with standard .SYNOPSIS, central BISF function couldn't used for services, SEP Service must being started with smc.exe
-		09.01.2017 MS: change code to get MacAdress to use function Get-BISMACAddress
+		09.01.2017 MS: Change code to get Mac address to use function Get-BISFMACAddress
 		01.07.2018 MS: Hotfix 49: After SEP is started with smc.exe, sometimes the service will not be started. Controlled and logged now with Test-BISFServiceState in Line 58
 		18.02.2020 JK: Fixed Log output spelling
-		19.02.2020 MS: HF 212 - SEP duplicate HardwareID - Get-BISFMacaddress returns lower- instead of uppercase MACAddress -> compare HardwareID after ServiceStart
+		19.02.2020 MS: HF 212 - SEP duplicate HardwareID - Get-BISFMacAddress returns lower- instead of uppercase MACAddress -> compare HardwareID after ServiceStart
 		14.04.2023 TR: HF 371 - Support both 32 bit and 64 bit locations. Reduce usage of global variables in functions. Use approved verbs.
 
-	.LINK
-		https://eucweb.com
 #>
 
 Begin {
@@ -42,10 +39,10 @@ Begin {
 	$PSScriptName = [System.IO.Path]::GetFileName($PSScriptFullName)
 
 	# define product
-	$reg_SEP_string = "Symantec\Symantec Endpoint Protection\SMC\SYLINK\SyLink"	
-	$reg2Check = "SerialNumber"
-	$reg_SEP_name = "HardwareID"
-	$HostID_Prfx = "00000000000000000000"	
+	$RegSEPString = "Symantec\Symantec Endpoint Protection\SMC\SYLINK\SyLink"	
+	$Reg2Check = "SerialNumber"
+	$RegSEPName = "HardwareID"
+	$HostIDPrfx = "00000000000000000000"	
 }
 
 Process {
@@ -66,10 +63,10 @@ Process {
 
 	function Get-SepSmcExePath
 	{
-		$sepPath = Get-SepPath
-		if($null -ne $sepPath)
+		$SepPath = Get-SepPath
+		if($null -ne $SepPath)
 		{
-			"$sepPath\smc.exe"
+			"$SepPath\smc.exe"
 		}
 		else {
 			$null
@@ -78,12 +75,12 @@ Process {
 
 	function Get-SepRegPath
 	{
-		if(Test-Path -Path "HKLM:\SOFTWARE\$reg_SEP_string")
+		if(Test-Path -Path "HKLM:\SOFTWARE\$RegSEPString")
 		{
-			"HKLM:\SOFTWARE\$reg_SEP_string"
+			"HKLM:\SOFTWARE\$RegSEPString"
 		}
-		elseif (Test-Path -Path "HKLM:\WOW6432Node\$reg_SEP_string") {
-			"HKLM:\WOW6432Node\$reg_SEP_string"
+		elseif (Test-Path -Path "HKLM:\WOW6432Node\$RegSEPString") {
+			"HKLM:\WOW6432Node\$RegSEPString"
 		}
 		else
 		{
@@ -93,13 +90,13 @@ Process {
 
 	function Test-SepIsInstalled
 	{
-		$regPath = Get-SepRegPath
-		$regPathExists = ($null -ne $regPath)
-		Write-Verbose "RegPathExists ('$regPath'): $regPathExists"
-		$smcExe = Get-SepSmcExePath
-		$smcExeExists = ($null -ne $smcExe) -and (Test-Path -Path $smcExe -PathType Leaf)
-		Write-Verbose "smcExeExists ('$smcExe'): $smcExeExists"
-		$regPathExists -and $smcExeExists
+		$RegPath = Get-SepRegPath
+		$RegPathExists = ($null -ne $RegPath)
+		Write-Verbose "RegPathExists ('$RegPath'): $RegPathExists"
+		$SmcExe = Get-SepSmcExePath
+		$SmcExeExists = ($null -ne $SmcExe) -and (Test-Path -Path $SmcExe -PathType Leaf)
+		Write-Verbose "smcExeExists ('$SmcExe'): $SmcExeExists"
+		$RegPathExists -and $SmcExeExists
 	}
 
 	function Test-SepHasSerialNumber
@@ -107,7 +104,7 @@ Process {
 		$SepRegPath = Get-SepRegPath
 		if($null -ne $SepRegPath)
 		{			
-			Test-BISFRegistryValue -Path $SepRegPath -Value $reg2Check
+			Test-BISFRegistryValue -Path $SepRegPath -Value $Reg2Check
 		}
 		else {
 			$false
@@ -116,7 +113,7 @@ Process {
 	
 	function Get-SepHostId
 	{
-		Get-ItemPropertyValue -Path $(Get-SepRegPath) -Name $reg_SEP_name
+		Get-ItemPropertyValue -Path $(Get-SepRegPath) -Name $RegSEPName
 	}
 
 	function Set-SepHostId
@@ -126,7 +123,7 @@ Process {
 			[string]
 			$RegHostID
 			)
-		Set-ItemProperty -Path $(Get-SepRegPath) -Name $reg_SEP_name -value $RegHostID -ErrorAction SilentlyContinue
+		Set-ItemProperty -Path $(Get-SepRegPath) -Name $RegSEPName -Value $RegHostID -ErrorAction SilentlyContinue
 	}
 
 	## set HostID in Registry
@@ -140,16 +137,16 @@ Process {
 
 		IF ($TestSEPinReg -eq $true) {
 			Write-BISFLog -Msg "Registry Location for specified SEP Keys will be set to $(Get-SepRegPath))"			
-			Write-BISFLog -Msg "$reg_SEP_name Prefix: $HostID_Prfx"
-			$regHostID = $HostID_Prfx + $MacAddress
-			Write-BISFLog -Msg "$reg_SEP_name will be defined as: $regHostID"
-			Write-BISFLog -Msg "set $reg_SEP_name in Registry $(Get-SepRegPath)"
-			Set-SEPHostId -RegHostID $regHostID | Out-Null
-			Write-Output -InputObject $regHostID
+			Write-BISFLog -Msg "$RegSEPName Prefix: $HostIDPrfx"
+			$RegHostID = $HostIDPrfx + $MacAddress
+			Write-BISFLog -Msg "$RegSEPName will be defined as: $RegHostID"
+			Write-BISFLog -Msg "set $RegSEPName in Registry $(Get-SepRegPath)"
+			Set-SEPHostId -RegHostID $RegHostID | Out-Null
+			Write-Output -InputObject $RegHostID
 		}
 		ELSE {
 			Write-BISFLog -Msg "Registry Location for specified SEP Keys could not be set to $(Get-SepRegPath)" -Type W -SubMsg
-			Write-BISFLog -Msg "The Value $reg2Check does not exist in the above location!" -Type W -SubMsg
+			Write-BISFLog -Msg "The Value $Reg2Check does not exist in the above location!" -Type W -SubMsg
 			Write-BISFLog -Msg "The SEP Service will be started, but each boot will create ghost entries in the SEP Management Server" -Type W -SubMsg
 			Write-Output -InputObject $null
 		}
@@ -169,11 +166,11 @@ Process {
 		Write-BISFLog -Msg "Start Service $($ServiceName)"
 		& "$(Get-SepSmcExePath)" "-start"
 		Test-BISFServiceState -ServiceName $ServiceName -Status "Running" | Out-Null
-		$testHardwareID = Get-SEPHostId
-		IF ($testHardwareID -eq $regHostID) {
-			Write-BISFLog -Msg "HardwareID in registry is set correctly: $testHardwareID"
+		$TestHardwareID = Get-SEPHostId
+		IF ($TestHardwareID -eq $RegHostID) {
+			Write-BISFLog -Msg "HardwareID in registry is set correctly: $TestHardwareID"
 		} ELSE {
-			Write-BISFLog -Msg "After the AV-Service is started, HardwareID in registry is NOT set correcty: Registry HardwareID $testHardwareID <-> Defined HardwareID '$regHostID'" -Type W -SubMsg
+			Write-BISFLog -Msg "After the AV-Service is started, HardwareID in registry is NOT set correctly: Registry HardwareID $TestHardwareID <-> Defined HardwareID '$RegHostID'" -Type W -SubMsg
 		}
 	}
 	####################################################################
