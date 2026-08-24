9d682555a92eae11c266211cde9f26d463917aef
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2026-08-24 01:23:58 -0400

    Refactor 10_PersBISF_SCOM.ps1 for improved variable naming and typo corrections. This update enhances variable names for consistency, corrects a typo in the synopsis, and aims to improve the overall readability and maintainability of the script while preserving its functionality.

--- Framework/SubCall/Personalization/10_PersBISF_SCOM.ps1
+++ Framework/SubCall/Personalization/10_PersBISF_SCOM.ps1
@@ -1,39 +1,36 @@
 ﻿<#
 	.SYNOPSIS
-		Personalize SCOM Client for Image Managemement Software
+		Personalize SCOM Client for Image Management Software
 	.DESCRIPTION
 
 	.EXAMPLE
 	.NOTES
 		Author: Matthias Schlimm
-	  	Company:  EUCWeb.com
 
 		History:
 	  	17.11.2014 MS: Script created for OpsMagr2k7
 		06.10.2015 MS: rewritten script with standard .SYNOPSIS, use central BISF function to configure service
 		04.03.2016 MS: fixed issue SCOM service would be start on every Image Mode if installed
-		19.10.2018 MS: Bugfix 72: MCS Deployment: SCOM Agent - creates OpsStateDir in C: drive
+		19.10.2018 MS: Fixed 72: MCS Deployment: SCOM Agent - creates OpsStateDir in C: drive
 		18.02.2020 JK: Fixed Log output spelling
 
-	.LINK
-		https://eucweb.com
 #>
 
 Begin {
 	$OpsStateDir = "$PVSDiskDrive\OpsStateDir"
-	$servicename = "HealthService"
+	$ServiceName = "HealthService"
 	$Product = "Microsoft SCOM Agent"
-	$script_path = $MyInvocation.MyCommand.Path
-	$script_dir = Split-Path -Parent $script_path
-	$script_name = [System.IO.Path]::GetFileName($script_path)
+	$ScriptPath = $MyInvocation.MyCommand.Path
+	$ScriptDir = Split-Path -Parent $ScriptPath
+	$ScriptName = [System.IO.Path]::GetFileName($ScriptPath)
 }
 
 Process {
 
-	$svc = Test-BISFService -ServiceName "$servicename" -ProductName "$Product"
-	IF ($svc) {
-		$OpsStateDir = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\services\$servicename\Parameters")."State Directory"
-		IF ($returnTestPVSSoftware -eq "true") {
+	$Svc = Test-BISFService -ServiceName "$ServiceName" -ProductName "$Product"
+	IF ($Svc) {
+		$OpsStateDir = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\services\$ServiceName\Parameters")."State Directory"
+		IF ($ReturnTestPVSSoftware -eq "true") {
 			Write-BISFLog -Msg "Citrix PVS Target Device detected, Set StateDirectory to Path $OpsStateDir"
 			If (!(Test-Path -Path $OpsStateDir)) {
 				Write-BISFLog -Msg "Create Directory $OpsStateDir"
@@ -43,7 +40,7 @@ Process {
 		ELSE {
 			Write-BISFLog -Msg "Citrix PVS Target Device NOT detected, leaving StateDirectory on original path $OpsStateDir"
 		}
-		Invoke-BISFService -ServiceName "$servicename" -Action Start
+		Invoke-BISFService -ServiceName "$ServiceName" -Action Start
 	}
 }
 
