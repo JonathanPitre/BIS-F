bfffa6fae3a2743e37d816439b2747a32f18d8c3
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2026-08-24 02:02:35 -0400

    Refactor 10_PrepBISF_AV-CylanceProtect.ps1 to correct typos in documentation and improve variable naming consistency. This update enhances clarity and readability while maintaining the script's functionality.

--- Framework/SubCall/Preparation/10_PrepBISF_AV-CylanceProtect.ps1
+++ Framework/SubCall/Preparation/10_PrepBISF_AV-CylanceProtect.ps1
@@ -1,6 +1,6 @@
 ﻿<#
     .SYNOPSIS
-        Prepare Cylance PROTECT Agent for Image Managemement
+        Prepare Cylance PROTECT Agent for Image Management
 	.DESCRIPTION
       	Delete computer specific entries
     .EXAMPLE
@@ -12,26 +12,23 @@
 			14.08.2019 MS: ENH 98: add function Set-CompatibilityMode
 			02.01.2020 MS: HF 164: Wrong Command for Compatibility Mode
 			01.06.2020 MS: HF 238: VDI Fingerprinting support
-			01.08.2020 MS: HF 261 - fix Errorhandling
-
-	.LINK
-        https://eucweb.com
+			01.08.2020 MS: HF 261 - fix error handling
 #>
 
 Begin {
-	$Script_Path = $MyInvocation.MyCommand.Path
-	$Script_Dir = Split-Path -Parent $Script_Path
-	$Script_Name = [System.IO.Path]::GetFileName($Script_Path)
+	$ScriptPath = $MyInvocation.MyCommand.Path
+	$ScriptDir = Split-Path -Parent $ScriptPath
+	$ScriptName = [System.IO.Path]::GetFileName($ScriptPath)
 
 	# Product specific parameters
 	$ProductName = "Cylance PROTECT"
 	$ProductPath = "${env:ProgramFiles}\Cylance\Desktop"
 	$ServiceName = "CylanceSvc"
 	[array]$ToDelete = @(
-		[pscustomobject]@{type = "REG"; value = "HKLM:\SOFTWARE\Cylance\Desktop"; data = "FP" },
-		[pscustomobject]@{type = "REG"; value = "HKLM:\SOFTWARE\Cylance\Desktop"; data = "FPMask" },
-		[pscustomobject]@{type = "REG"; value = "HKLM:\SOFTWARE\Cylance\Desktop"; data = "FPVersion" },
-		[pscustomobject]@{type = "REG"; value = "HKLM:\SOFTWARE\Cylance\Desktop"; data = "SelfProtectionLevel" }
+		[PSCustomObject]@{type = "REG"; value = "HKLM:\SOFTWARE\Cylance\Desktop"; data = "FP" },
+		[PSCustomObject]@{type = "REG"; value = "HKLM:\SOFTWARE\Cylance\Desktop"; data = "FPMask" },
+		[PSCustomObject]@{type = "REG"; value = "HKLM:\SOFTWARE\Cylance\Desktop"; data = "FPVersion" },
+		[PSCustomObject]@{type = "REG"; value = "HKLM:\SOFTWARE\Cylance\Desktop"; data = "SelfProtectionLevel" }
 	)
 }
 
@@ -46,7 +43,7 @@ Process {
 		Write-BISFLog -Msg "Delete specified items "
 		Foreach ($DeleteItem in $ToDelete) {
 			IF ($DeleteItem.type -eq "REG") {
-				Write-BISFLog -Msg "Processing registry item to delete" -ShowConsole -SubMsg -color DarkCyan
+				Write-BISFLog -Msg "Processing registry item to delete" -ShowConsole -SubMsg -Color DarkCyan
 				$VerifyRegistryItem = Test-BISFRegistryValue -Path $DeleteItem.value -Value $DeleteItem.data
 				IF ($VerifyRegistryItem) {
 					Write-BISFLog -Msg "Deleting registry item -Path($DeleteItem.value) -Name($DeleteItem.data)"
@@ -55,7 +52,7 @@ Process {
 			}
 
 			IF ($DeleteItem.type -eq "FILE") {
-				Write-BISFLog -Msg "Processing file item to delete" -ShowConsole -SubMsg -color DarkCyan
+				Write-BISFLog -Msg "Processing file item to delete" -ShowConsole -SubMsg -Color DarkCyan
 				$FullFileName = "$DeleteItem.value\$DeleteItem.data"
 				IF (Test-Path ($FullFileName) -PathType Leaf) {
 					Write-BISFLog -Msg "Deleting File $FullFileName"
@@ -65,8 +62,8 @@ Process {
 		}
 	}
 	function Stop-Service {
-		$svc = Test-BISFService -ServiceName "$ServiceName"
-		IF ($svc -eq $true) { Invoke-BISFService -ServiceName "$($ServiceName)" -Action Stop }
+		$Svc = Test-BISFService -ServiceName "$ServiceName"
+		IF ($Svc -eq $true) { Invoke-BISFService -ServiceName "$($ServiceName)" -Action Stop }
 	}
 
 	function Set-CompatibilityMode {
@@ -83,23 +80,23 @@ Process {
 			Author: Matthias Schlimm
 
 				14.08.2019 MS: function created
-				01.08.2020 MS: HF 261 - fix Errorhandling
+				01.08.2020 MS: HF 261 - fix error handling
 		#>
 
 		$CompatibilityMode = (Get-ItemProperty HKLM:\SOFTWARE\Cylance\Desktop).CompatibilityMode
         IF ($CompatibilityMode -ne 0) {
             $ErrorActionPreference = "Stop"
             Write-BISFLog -Msg "Take Registry Ownership" -ShowConsole -Color DarkCyan -SubMsg
-		    #Adjust current user privilegs
-		    $null = enable-BISFprivilege SeTakeOwnershipPrivilege
+		    #Adjust current user privileges
+		    $null = Enable-BISFprivilege -Privilege SeTakeOwnershipPrivilege
 
 		    #Take Ownership of Registry Key
-		    $key = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey("SOFTWARE\Cylance\Desktop", [Microsoft.Win32.RegistryKeyPermissionCheck]::ReadWriteSubTree, [System.Security.AccessControl.RegistryRights]::takeownership)
+		    $Key = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey("SOFTWARE\Cylance\Desktop", [Microsoft.Win32.RegistryKeyPermissionCheck]::ReadWriteSubTree, [System.Security.AccessControl.RegistryRights]::TakeOwnership)
 		    try {
-			    $acl = $key.GetAccessControl([System.Security.AccessControl.AccessControlSections]::None)
-                $me = [System.Security.Principal.NTAccount]"$env:username"
-		        $acl.SetOwner($me)
-		        $key.SetAccessControl($acl)
+			    $Acl = $Key.GetAccessControl([System.Security.AccessControl.AccessControlSections]::None)
+                $Me = [System.Security.Principal.NTAccount]"$env:username"
+		        $Acl.SetOwner($Me)
+		        $Key.SetAccessControl($Acl)
 		    }
 
             catch {
@@ -108,13 +105,13 @@ Process {
 
 
 
-		    #Read current ACL and add rule for Builtin\Admnistrators
+		    #Read current ACL and add rule for Builtin\Administrators
 		    try {
-                $acl = $key.GetAccessControl()
-		        $rule = New-Object System.Security.AccessControl.RegistryAccessRule ("$env:username", "FullControl", "Allow")
-		        $acl.SetAccessRule($rule)
-		        $key.SetAccessControl($acl)
-		        $key.Close()
+                $Acl = $Key.GetAccessControl()
+		        $Rule = New-Object System.Security.AccessControl.RegistryAccessRule ("$env:username", "FullControl", "Allow")
+		        $Acl.SetAccessRule($Rule)
+		        $Key.SetAccessControl($Acl)
+		        $Key.Close()
             }
 
             catch {
@@ -124,7 +121,7 @@ Process {
 
 		    Write-BISFLog -Msg "Set Compatibility Mode" -ShowConsole -Color DarkCyan -SubMsg
 		    try {
-                New-ItemProperty -Path "HKLM:\SOFTWARE\Cylance\Desktop" -Name "CompatibilityMode" -value 01 -PropertyType Binary -Force
+                New-ItemProperty -Path "HKLM:\SOFTWARE\Cylance\Desktop" -Name "CompatibilityMode" -Value 01 -PropertyType Binary -Force
              }
 
             catch {
@@ -141,8 +138,8 @@ Process {
 	####################################################################
 
 	#### Main Program
-	$svc = Test-BISFService -ServiceName $ServiceName -ProductName $ProductName
-	If ($svc -eq $true) {
+	$Svc = Test-BISFService -ServiceName $ServiceName -ProductName $ProductName
+	If ($Svc -eq $true) {
 		Write-BISFLog -Msg "Product $ProductName installed" -ShowConsole -Color Cyan
 		$VDIType = (Get-ItemProperty HKLM:\SOFTWARE\Cylance\Desktop).VDIType
 		IF (!($VDIType -eq 0)) {
