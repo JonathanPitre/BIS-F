7eb3a0ba5f4e913a0b03d1ac9b135890cb73fcb8
Jonathan Pitre <15003329+JonathanPitre@users.noreply.github.com>
2026-08-24 01:48:00 -0400

    Refactor 10_PrepBISF_Splunk.ps1 for improved variable naming and typo corrections. This update enhances variable names for consistency, corrects a typo in the synopsis, and improves overall readability and maintainability of the script while preserving its functionality.

--- Framework/SubCall/Preparation/10_PrepBISF_Splunk.ps1
+++ Framework/SubCall/Preparation/10_PrepBISF_Splunk.ps1
@@ -1,39 +1,35 @@
 ﻿<#
 	.SYNOPSIS
-		Prepare Splunk Universal Fowarder for Image Management
+		Prepare Splunk Universal Forwarder for Image Management
 	.DESCRIPTION
 	  	Delete computer specific entries
 	.EXAMPLE
 	.NOTES
 		Author: Matthias Schlimm
-	  	Company:  EUCWeb.com
 
 		History:
 	  	15.12.2014 JP: Script created
 		06.02.2015 MS: review script
 		01.10.2015 MS: rewritten script with standard .SYNOPSIS, use central BISF function to configure service
-		28.05.2018 MS: Bugfix 41: Set SplunkForwarder to StartType Automatic
-
-	.LINK
-		https://eucweb.com
+		28.05.2018 MS: Fixed 41: Set SplunkForwarder to StartType Automatic
 #>
 
 Begin {
-	$script_path = $MyInvocation.MyCommand.Path
-	$script_dir = Split-Path -Parent $script_path
-	$script_name = [System.IO.Path]::GetFileName($script_path)
+	$ScriptPath = $MyInvocation.MyCommand.Path
+	$ScriptDir = Split-Path -Parent $ScriptPath
+	$ScriptName = [System.IO.Path]::GetFileName($ScriptPath)
 	$Product = "Splunk Universal Forwarder"
-	$product_path = "${env:ProgramFiles}\SplunkUniversalForwarder\bin"
-	$servicename = "SplunkForwarder"
+	$ProductPath = "${env:ProgramFiles}\SplunkUniversalForwarder\bin"
+	$ServiceName = "SplunkForwarder"
 }
 
 Process {
 
-	$svc = Test-BISFService -ServiceName "$servicename" -ProductName "$product"
-	IF ($svc -eq $true) {
-		Invoke-BISFService -ServiceName "$servicename" -Action Stop -StartType Automatic
+	$Svc = Test-BISFService -ServiceName "$ServiceName" -ProductName "$Product"
+	IF ($Svc -eq $true) {
+		Invoke-BISFService -ServiceName "$ServiceName" -Action Stop -StartType Automatic
 		Write-BISFLog -Msg "Clear $Product config"
-		& Start-Process -FilePath "$product_path\splunk.exe" -ArgumentList "clone-prep-clear-config" -Wait -WindowStyle Hidden
+		& Start-Process -FilePath "$ProductPath\splunk.exe" -ArgumentList "clone-prep-clear-config" -Wait -WindowStyle Hidden
 	}
 }
 
